f305816468c3677ccabf7b0d4dcd99001c6084ef
grumbel <grumbel@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2004-08-01 14:05:07 +0000

    - added missing includes
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@127 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- tuxkart/src/pwdrv.cxx
+++ tuxkart/src/pwdrv.cxx
@@ -1,4 +1,4 @@
-//  $Id: pwdrv.cxx,v 1.3 2004/07/31 23:46:18 grumbel Exp $
+//  $Id: pwdrv.cxx,v 1.4 2004/08/01 14:05:07 grumbel Exp $
 //
 //  TuxKart - a fun racing game with go-kart
 //  Copyright (C) 2004 Steve Baker <sjbaker1@airmail.net>
@@ -17,6 +17,8 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+#include <plib/pu.h>
+#include "status.h" 
 #include "pwdrv.h"
 #include "tuxkart.h"
 
