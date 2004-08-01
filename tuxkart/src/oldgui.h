c7ff51e5543c23fff8140918bbcb230f4df55b63
jamesgregory <jamesgregory@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2004-08-01 18:49:24 +0000

    New files for a new GUI
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@133 178a84e3-b1eb-0310-8ba1-8eac791a3b58

---
+++ tuxkart/src/oldgui.h
@@ -0,0 +1,21 @@
+
+#include <plib/pu.h>
+
+class OldGUI
+{
+private:
+  class puMenuBar *main_menu_bar ;
+
+  int hidden ;
+
+public:
+
+  OldGUI () ;
+  void update () ;
+  void show   () ;
+  void hide   () ;  
+  int  isHidden () { return hidden ; }
+} ;
+
+extern fntTexFont *oldfont ;
+
