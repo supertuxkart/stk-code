7ee89360609b6481d6d462b6a2163daddfc1a80e
grumbel <grumbel@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2004-08-02 13:34:15 +0000

    - added a banana as (temporary) green herring replacement
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@151 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- tuxkart/src/tuxkart.cxx
+++ tuxkart/src/tuxkart.cxx
@@ -1,4 +1,4 @@
-//  $Id: tuxkart.cxx,v 1.32 2004/08/02 13:03:32 matzebraun Exp $
+//  $Id: tuxkart.cxx,v 1.33 2004/08/02 13:34:15 grumbel Exp $
 //
 //  TuxKart - a fun racing game with go-kart
 //  Copyright (C) 2004 Steve Baker <sjbaker1@airmail.net>
@@ -507,13 +507,15 @@ int tuxkartMain ( int _numLaps, int _mirror, int _reverse,
   sgVec3 cyan   = { 0.4, 1.0, 1.0 } ;
   sgVec3 yellow = { 1.0, 1.0, 0.4 } ;
   //sgVec3 red    = { 0.8, 0.0, 0.0 } ;
-  sgVec3 green  = { 0.0, 0.8, 0.0 } ;
+  //sgVec3 green  = { 0.0, 0.8, 0.0 } ;
  
-  silver_h  = new Herring ( cyan   ) ; preProcessObj ( silver_h -> getRoot(), mirror ) ;
-    gold_h    = new Herring ( yellow ) ; preProcessObj ( gold_h -> getRoot(), mirror ) ;
   //red_h     = new Herring ( red    ) ; preProcessObj ( red_h -> getRoot(), mirror ) ;
+  //green_h   = new Herring ( green  ) ; preProcessObj ( green_h -> getRoot(), mirror ) ;
+
+  silver_h  = new Herring ( cyan   ) ; preProcessObj ( silver_h -> getRoot(), mirror ) ;
+  gold_h    = new Herring ( yellow ) ; preProcessObj ( gold_h -> getRoot(), mirror ) ;
   red_h     = new Herring ( ssgLoad ( "bonusblock.ac", loader )   ) ; preProcessObj ( red_h -> getRoot(), mirror ) ;
-  green_h   = new Herring ( green  ) ; preProcessObj ( green_h -> getRoot(), mirror ) ;
+  green_h   = new Herring ( ssgLoad ( "banana.ac", loader )   ) ; preProcessObj ( red_h -> getRoot(), mirror ) ;
 
   /* Load the Karts */
 
