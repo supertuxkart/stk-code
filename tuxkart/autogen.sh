eba542873cfd1930a41bd59daf8d036e25e0c8ca
grumbel <grumbel@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2004-08-09 11:39:28 +0000

    - added some magic to check for right automake version
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@251 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- tuxkart/autogen.sh
+++ tuxkart/autogen.sh
@@ -2,10 +2,30 @@
 
 set -e
 
-echo "Running aclocal"
-aclocal -I m4
-echo "Running automake"
-automake --add-missing --copy
+WANT_AUTOMAKE=1.7
+
+if [ -n "`which automake-1.7`" ]; then
+    ACLOCAL="aclocal-1.7"
+    AUTOMAKE="automake-1.7"
+elif [ -n "`which automake-1.6`" ]; then
+    ACLOCAL="aclocal-1.6"
+    AUTOMAKE="automake-1.6"
+else
+    ACLOCAL="aclocal"
+    AUTOMAKE="automake"
+fi
+
+case "`$AUTOMAKE --version`" in
+    *1.5* | *1.4*)
+        echo "Error: You need automake 1.6 or larger"
+        exit 1
+        ;;
+esac
+
+echo "Running $ACLOCAL"
+$ACLOCAL -I m4
+echo "Running $AUTOMAKE"
+$AUTOMAKE --add-missing --copy
 echo "Running autoconf"
 autoconf 
 
