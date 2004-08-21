c3bf6b01dc72542feb1d6d38b4b8c2c99313c9f1
jamesgregory <jamesgregory@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2004-08-21 17:28:02 +0000

    Fixed typo in command line info message
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@367 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- tuxkart/src/start_tuxkart.cxx
+++ tuxkart/src/start_tuxkart.cxx
@@ -1,4 +1,4 @@
-//  $Id: start_tuxkart.cxx,v 1.68 2004/08/20 22:50:54 jamesgregory Exp $
+//  $Id: start_tuxkart.cxx,v 1.69 2004/08/21 17:28:02 jamesgregory Exp $
 //
 //  TuxKart - a fun racing game with go-kart
 //  Copyright (C) 2004 Steve Baker <sjbaker1@airmail.net>
@@ -110,7 +110,7 @@ void cmdLineHelp (char* invocation)
 	    "  -l,  --list-tracks      Show available tracks.\n"
 	    "  -k,  --numkarts NUM     Number of karts on the racetrack\n"
 	    "  --kart n                Use kart number n\n"
-	    "  ---list-karts           Show available karts.\n"
+	    "  --list-karts            Show available karts.\n"
 	    "  --laps n                Define number of laps to n\n"
 	    "  --players n             Define number of players to between 1 and 4.\n"
 	    "  --reverse               Enable reverse mode\n"
