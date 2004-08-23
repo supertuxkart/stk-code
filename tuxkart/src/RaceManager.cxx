8410ca3eb7640b20bb418fb6dde1722e2728fd2c
grumbel <grumbel@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2004-08-23 19:20:49 +0000

    - cxx/cpp change on wrong filename
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@390 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- tuxkart/src/RaceManager.cxx
+++ tuxkart/src/RaceManager.cxx
@@ -1,4 +1,4 @@
-//  $Id: RaceManager.cpp,v 1.1 2004/08/23 18:20:40 grumbel Exp $
+//  $Id: RaceManager.cxx,v 1.1 2004/08/23 19:20:49 grumbel Exp $
 //
 //  TuxKart - a fun racing game with go-kart
 //  Copyright (C) 2004 Steve Baker <sjbaker1@airmail.net>
@@ -23,6 +23,12 @@ RaceManager* RaceManager::instance_ = 0;
 
 RaceManager::RaceManager()
 {  
+}
+
+void
+RaceManager::next_race()
+{
+  
 }
 
 /* EOF */
