9599b5b1597673e5cd1b73dbbbbed5d96d8691e4
grumbel <grumbel@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2004-08-23 16:44:47 +0000

    - added manager class for grandprix stuff
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@386 178a84e3-b1eb-0310-8ba1-8eac791a3b58

---
+++ tuxkart/src/GrandPrixSetup.h
@@ -0,0 +1,53 @@
+//  $Id: GrandPrixSetup.h,v 1.1 2004/08/23 16:44:47 grumbel Exp $
+//
+//  TuxKart - a fun racing game with go-kart
+//  Copyright (C) 2004 Ingo Ruhnke <grumbel@gmx.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef HEADER_GRANDPRIXSETUP_H
+#define HEADER_GRANDPRIXSETUP_H
+
+#include <vector>
+#include <string>
+
+class GrandPrixSetup
+{
+public:
+  struct Stat {
+    Stat();
+
+    /** ident of the kart (aka. .tkkf filename without .tkkf) */
+    std::string ident;
+
+    /** Number of Points optained in the GrandPrix so far */
+    int points;
+    
+    /** Position of the kart in the next race (equal to the finishing
+        position in the last race) */
+    int position;
+  };
+
+  GrandPrixSetup();
+  ~GrandPrixSetup();
+
+  /** The karts that participate in the GrandPrix and there point,
+      position, etc. */
+  std::vector<Stat> karts;
+};
+
+#endif
+
+/* EOF */
