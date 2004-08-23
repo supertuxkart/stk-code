b5b3993eababc3806516c8265264292a6b459670
grumbel <grumbel@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2004-08-23 14:00:04 +0000

    - added some extremly simple class to hold CupData
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@382 178a84e3-b1eb-0310-8ba1-8eac791a3b58

---
+++ tuxkart/src/CupData.cxx
@@ -0,0 +1,43 @@
+//  $Id: CupData.cxx,v 1.1 2004/08/23 14:00:04 grumbel Exp $
+//
+//  TuxKart - a fun racing game with go-kart
+//  Copyright (C) 2004 Steve Baker <sjbaker1@airmail.net>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#include <iostream>
+#include "Loader.h"
+#include "lispreader.h"
+#include "CupData.h"
+
+CupData::CupData(const std::string& filename)
+{
+  try {
+    LispReader* cup = LispReader::load(loader ? loader->getPath(filename) : filename, "tuxkart-cup");
+    assert(cup);
+    
+    LispReader reader(cup->get_lisp());
+    reader.read_string("name", name);
+    reader.read_string_vector("tracks", tracks);
+    
+    delete cup;
+  } catch(LispReaderException& err) {
+    std::cout << "CupData: LispReaderException: " << err.message << std::endl;
+  } catch(std::exception& err) {
+    std::cout << "CupData: Catched std::exception: " << err.what() << std::endl;
+  }
+}
+
+/* EOF */
