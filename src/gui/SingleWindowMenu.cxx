1610e2f690bd9588aa8f9402d6b1d80ed1595ece
hiker <hiker@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2006-05-10 04:47:28 +0000

    Fixed all cygwin related issues, so that no more work arounds are necessary (the order in which pw.h and pu.h are included appears to be important!).
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@594 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/gui/SingleWindowMenu.cxx
+++ src/gui/SingleWindowMenu.cxx
@@ -18,8 +18,8 @@
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <signal.h>
-#include <plib/pu.h>
 #include <plib/pw.h>
+#include <plib/pu.h>
 #include "SingleWindowMenu.h"
 #include "TrackManager.h"
 #include "Track.h"
