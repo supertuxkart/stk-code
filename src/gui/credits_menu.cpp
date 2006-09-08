32e84f0b687ede0331a1610770376d71fe45eb32
hiker <hiker@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2006-09-08 04:16:42 +0000

    Added some error handling.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@745 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/gui/credits_menu.cpp
+++ src/gui/credits_menu.cpp
@@ -19,18 +19,24 @@
 
 #include "credits_menu.hpp"
 #include "loader.hpp"
-
+#include <stdexcept>
 CreditsMenu::CreditsMenu(){
 
-  std::string filename = loader->getPath("data/CREDITS");
-  FILE *fd = fopen(filename.c_str(), "r");
-  
-  char s[1024];
-  while(fgets(s, 1023, fd)) {
-    char *p = strdup(s);
-    sl.push_back(p);
+  std::string filename;
+  try {
+    filename = loader->getPath("data/CREDITS");
+    FILE *fd = fopen(filename.c_str(), "ra");
+    char s[1024];
+    while(fgets(s, 1023, fd)) {
+      char *p = strdup(s);
+      sl.push_back(p);
+    }   // while
+  } catch(std::runtime_error& e) {
+    printf("Couldn't load '%s'\n",filename.c_str());
+    sl.push_back("CREDIT file was not installed properly!!");
+    sl.push_back("Please check 'data/CREDIT'!!");
   }
-  fclose(fd);
+  
   setText(sl);
 }   // CreditsMenu
 
