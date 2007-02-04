316341f6c8fc34c44dec5543175cb87b3c2e463a
coz <coz@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2007-02-04 02:35:18 +0000

    Removed two unneeded includes.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@908 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/gui/help_menu.cpp
+++ src/gui/help_menu.cpp
@@ -17,9 +17,6 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#include "kart_properties_manager.hpp"
-#include "kart_properties.hpp"
-
 #include "help_menu.hpp"
 #include "widget_set.hpp"
 #include "race_manager.hpp"
