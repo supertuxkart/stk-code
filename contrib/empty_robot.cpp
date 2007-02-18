f009ba17fa41ae15123fdd0e2c48931020e93fb8
coz <coz@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2007-02-18 05:13:33 +0000

    Added a template to create new robots.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@912 178a84e3-b1eb-0310-8ba1-8eac791a3b58

---
+++ contrib/empty_robot.cpp
@@ -0,0 +1,71 @@
+/* This is a template to avoid writing boring stuff again and again when
+ * creating a new robot.
+ *
+ * Don't forget to replace the copyright in the fourth line of the header in
+ * this file and the header file, with the current year and your name, the
+ * #include to <name of your robot>_robot.hpp, the class name references in
+ * this file and in empty_robot.hpp to <robot name>Robot and the #ifdef at
+ * beginning of the header file.
+ *
+ * You should also delete these intructions. Oh, and if you use this template
+ * as a robot, it does nothing.
+ */
+
+//  SuperTuxKart - a fun racing game with go-kart
+//  Copyright (C) 2004-2005 Steve Baker <sjbaker1@airmail.net>
+//  Copyright (C) 2006-2007 Eduardo Hernandez Munoz
+//  Copyright (C) <insert year here> <insert name here>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#include "world.hpp"
+
+#include "empty_robot.hpp"
+
+EmptyRobot::EmptyRobot(const KartProperties *kart_properties, int position,
+                   sgCoord init_pos) :
+    AutoKart(kart_properties, position, init_pos)
+{
+    //This is called just once per *competition*
+}
+
+//-----------------------------------------------------------------------------
+void EmptyRobot::update (float delta)
+{
+    if( world->getPhase() == World::START_PHASE )
+    {
+        handle_race_start();
+        return;
+    }
+
+    /*General kart stuff*/
+    Kart::update(delta);
+}
+
+//-----------------------------------------------------------------------------
+void EmptyRobot::handle_race_start()
+{
+        //FIXME: is this next line really needed?
+        placeModel();
+}
+
+//-----------------------------------------------------------------------------
+void EmptyRobot::reset()
+{
+    //This function is called at the beginning of *each race*
+
+    /*General kart stuff*/
+    Kart::reset();
+}
