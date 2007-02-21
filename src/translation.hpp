97bad76cebc67b77e7679db6b697b482d02b4aa4
hiker <hiker@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2007-02-21 08:37:32 +0000

    Bugfix: Using LC_ALL prevents sscanf to correctly parse drivelines.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk@916 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/translation.hpp
+++ src/translation.hpp
@@ -20,7 +20,7 @@
 #ifndef TRANSLATION_H
 #define TRANLSATION_H
 
-#undef HAS_GETTEXT
+#define HAS_GETTEXT
 
 #ifdef HAS_GETTEXT
 #  include <libintl.h>
@@ -46,10 +46,13 @@ public:
 #ifdef HAS_GETTEXT
     initTranslations() 
     { 
-        //        setlocale (LC_ALL, "");
         //        bindtextdomain (PACKAGE, LOCALEDIR);
         //        textdomain (PACKAGE);
-        setlocale (LC_ALL, "");
+        // LC_ALL does not work, sscanf will then not always be able
+        // to scan for example: s=-1.1,-2.3,-3.3 correctly, which is
+        // used in driveline files.
+        setlocale (LC_CTYPE,    "");
+        setlocale (LC_MESSAGES, "");
         bindtextdomain (PACKAGE, "/home/joh/local/tuxkart/po");
         textdomain (PACKAGE);
     }
