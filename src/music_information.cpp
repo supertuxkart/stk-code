6e336a379e0a426b428434de892c8f5e34d45596
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2008-03-14 10:49:23 +0000

    Bugfixes for linux compilation.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk/supertuxkart@1653 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/music_information.cpp
+++ src/music_information.cpp
@@ -18,6 +18,8 @@
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 
+#include <stdexcept>
+
 #include "music_information.hpp"
 #include "lisp/lisp.hpp"
 #include "lisp/parser.hpp"
