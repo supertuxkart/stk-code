3c3d25cb86ccc21e6df852ef1184743ae8311477
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2008-05-09 18:39:48 +0000

    improved .pot generator script
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk/supertuxkart@1840 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- data/po/update_pot.sh
+++ data/po/update_pot.sh
@@ -2,4 +2,15 @@
 #
 # ./data/po/update_pot.sh
 
+CPP_FILE_LIST=`find ./src -name '*.cpp' -print`
+
+echo "--------------------"
+echo "    Files :"
+echo "--------------------"
+echo $CPP_FILE_LIST
+
+echo "---------------------------"
+echo "    Generating .pot file..."
 xgettext -d supertuxkart -s --keyword=_ -p ./data/po -o supertuxkart.pot ./src/*.cpp ./src/*/*.cpp
+echo "    Done"
+echo "---------------------------"
