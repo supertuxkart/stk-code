eb482c7d4b09c4f85c78660e550c22f57a0ccfc4
cosmosninja <cosmosninja@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2008-06-13 00:53:52 +0000

    Updated the GPL version to GPLv3.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk/supertuxkart@2128 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/bullet/Demos/OpenGL/BMF_BitmapFont.cpp
+++ src/bullet/Demos/OpenGL/BMF_BitmapFont.cpp
@@ -3,7 +3,7 @@
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU bteral Public License
- * as published by the Free Software Foundation; either version 2
+ * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version. The Blender
  * Foundation also sells licenses for use in proprietary software under
  * the Blender License.  See http://www.blender.org/BL/ for information
