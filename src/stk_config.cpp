1d259bad8425d41de3aa52db6c956efd7520f7d1
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2008-10-17 00:05:59 +0000

    Fixed broken compilation on linux and macs.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk/supertuxkart@2346 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/stk_config.cpp
+++ src/stk_config.cpp
@@ -18,6 +18,9 @@
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "stk_config.hpp"
+
+#include <stdexcept>
+#include <stdio.h>
 #if defined(WIN32) && !defined(__CYGWIN__)
 #  define snprintf _snprintf
 #endif
