45e00400a383bb211a4318d2ac3ed97f804a7e68
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2008-12-03 06:13:02 +0000

    Minor improvements to nitro graphics.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk/supertuxkart@2563 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/graphics/nitro.cpp
+++ src/graphics/nitro.cpp
@@ -77,8 +77,8 @@ void Nitro::particle_create(int, Particle *p)
     const Vec3 vel = -m_kart->getVelocity()*0.2f;
     sgCopyVec3(p->m_vel, vel.toFloat());
     sgCopyVec3(p->m_pos, xyz.toFloat());
-    p->m_vel[0] += cos(DEGREE_TO_RAD(rand()% 10));
-    p->m_vel[1] += sin(DEGREE_TO_RAD(rand()% 80));
+    p->m_vel[0] += 2*cos(DEGREE_TO_RAD(rand()% 180));
+    p->m_vel[1] += 2*sin(DEGREE_TO_RAD(rand()% 180));
     p->m_vel[2] = 0;
 }   // particle_create
 
