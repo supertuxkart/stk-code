09e275b3a08280f405ec92266924169568f664e6
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2009-01-25 19:57:13 +0000

    fixed compilation under GCC 4.4
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/trunk/supertuxkart@3037 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/lisp/lexer.cpp
+++ src/lisp/lexer.cpp
@@ -19,6 +19,7 @@
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <sstream>
 #include <stdexcept>
+#include <cstdio>
 #include <cstring>
 
 #include "lisp/lexer.hpp"
