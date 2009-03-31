721348c2ce8adb32f535dc7b48a0bd4e3a565fee
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2009-03-31 20:42:32 +0000

    added debug check to help minibjorn find the cause of his issues with gamepad
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/branches/irrlicht@3332 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/input/input_device.cpp
+++ src/input/input_device.cpp
@@ -267,6 +267,7 @@ void GamePadDevice::resetAxisDirection(const int axis, Input::AxisDirection dire
 // -----------------------------------------------------------------------------
 bool GamePadDevice::hasBinding(Input::InputType type, const int id, const int value, const int player, PlayerAction* action /* out */)
 {
+    printf("Something's triggered in gamepad\n");
     if(type == Input::IT_STICKMOTION)
     {
         // going to negative from positive
@@ -332,6 +333,7 @@ bool GamePadDevice::hasBinding(Input::InputType type, const int id, const int va
     }
     else if(type == Input::IT_STICKBUTTON)
     {
+        printf(" It's a button press, #%i\n", id);
         // find corresponding action and return it
         for(int n=0; n<PA_COUNT; n++)
         {
