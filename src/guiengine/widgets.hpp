f856a11031b38be89d6060261ce015dd85b2bee9
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2009-07-19 00:27:42 +0000

    Added forgotten file
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/branches/irrlicht@3768 178a84e3-b1eb-0310-8ba1-8eac791a3b58

---
+++ src/guiengine/widgets.hpp
@@ -0,0 +1,13 @@
+
+// A shortcut header to get all widgets
+
+#include "guiengine/widgets/button_widget.hpp"
+#include "guiengine/widgets/icon_button_widget.hpp"
+#include "guiengine/widgets/list_widget.hpp"
+#include "guiengine/widgets/ribbon_grid_widget.hpp"
+#include "guiengine/widgets/spinner_widget.hpp"
+#include "guiengine/widgets/ribbon_widget.hpp"
+#include "guiengine/widgets/model_view_widget.hpp"
+#include "guiengine/widgets/text_box_widget.hpp"
+#include "guiengine/widgets/label_widget.hpp"
+#include "guiengine/widgets/check_box_widget.hpp"
