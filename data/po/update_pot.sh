b6d0226fe2ded2cf33ce420e17848dacd28594aa
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2009-08-29 21:16:51 +0000

    Updated pot generation script to fetch strings from XML too (most of them are not translated in game yet however)
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/branches/irrlicht@3957 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- data/po/update_pot.sh
+++ data/po/update_pot.sh
@@ -4,19 +4,39 @@
 
 CPP_FILE_LIST=`find ./src -name '*.cpp' -print`
 PERL_FILE_LIST="`find ./data -name '*.track' -print` `find ./data -name '*.challenge' -print` `find ./data -name '*.grandprix' -print`"
+XML_FILE_LIST=`find ./data -name '*.xml' -print`
+STKGUI_FILE_LIST=`find ./data -name '*.stkgui' -print`
 
 echo "--------------------"
 echo "    Source Files :"
 echo "--------------------"
 echo $CPP_FILE_LIST
+
 echo "--------------------"
 echo "    Data Files :"
 echo "--------------------"
 echo $PERL_FILE_LIST
 
+echo "--------------------"
+echo "    XMl Files :"
+echo "--------------------"
+echo $XML_FILE_LIST $STKGUI_FILE_LIST
+
+
+
 echo "---------------------------"
 echo "    Generating .pot file..."
+
+# C++ Files
 xgettext    -d supertuxkart -s --keyword=_ --add-comments="I18N:" -p ./data/po -o supertuxkart.pot $CPP_FILE_LIST
+
+# Lisp files
 xgettext -j -L perl -d supertuxkart -s --keyword=_ --add-comments="I18N:" -p ./data/po -o supertuxkart.pot $PERL_FILE_LIST
+
+# XML Files
+# (Since xgettext cannot read XML directly, strings are extracted using 'grep' and 'sed' first
+grep -ho 'text=\"[^\"]*' $XML_FILE_LIST $STKGUI_FILE_LIST | sed 's/text=\"\(.*\)/_(\"\1\")/' > ./data/po/gui_strings.txt
+xgettext -j -L perl -d supertuxkart -s --keyword=_ --add-comments="I18N:" -p ./data/po -o supertuxkart.pot ./data/po/gui_strings.txt
+
 echo "    Done"
 echo "---------------------------"
