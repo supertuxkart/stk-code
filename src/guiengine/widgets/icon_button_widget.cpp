ace9f372aabaac0a555dd11f1e4e9feeb143c673
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2009-10-12 22:53:44 +0000

    Removed another bogus include
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/branches/irrlicht@4109 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/guiengine/widgets/icon_button_widget.cpp
+++ src/guiengine/widgets/icon_button_widget.cpp
@@ -16,7 +16,6 @@
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "guiengine/engine.hpp"
-#include "guiengine/my_button.hpp"
 #include "guiengine/widgets/icon_button_widget.hpp"
 #include "io/file_manager.hpp"
 using namespace GUIEngine;
