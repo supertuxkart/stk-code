6d89dd7ae0cb9ebf7e8f62bfc0b3dc3449f2a34d
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2009-11-08 12:15:12 +0000

    Use loadRobot instead of new DefaultRobot, so that the same function is used to create all AI karts.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/branches/irrlicht@4212 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/modes/profile_world.cpp
+++ src/modes/profile_world.cpp
@@ -81,7 +81,8 @@ Kart *ProfileWorld::createKart(const std::string &kart_ident, int index,
 {
     // Create a camera for the last kart (since this way more of the
     // karts can be seen.
-    Kart *newkart = new DefaultRobot(kart_ident, index+1, init_pos, m_track);
+    Kart *newkart = loadRobot(kart_ident, index+1, init_pos);
+
     if (index == (int)race_manager->getNumKarts()-1)
     {
         // The pointer to the camera does not have to be stored, since it
