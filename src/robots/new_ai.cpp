f80dc3a26bfe04bf17d55ef26af5b9e9b2f5ebd6
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2009-11-14 22:57:57 +0000

    Disabled debug spheres to be shown.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/branches/irrlicht@4225 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/robots/new_ai.cpp
+++ src/robots/new_ai.cpp
@@ -20,7 +20,7 @@
 
 //The AI debugging works best with just 1 AI kart, so set the number of karts
 //to 2 in main.cpp with quickstart and run supertuxkart with the arg -N.
-#define AI_DEBUG
+#undef AI_DEBUG
 
 #include "robots/new_ai.hpp"
 
