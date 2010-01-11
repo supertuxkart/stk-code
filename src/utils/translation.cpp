1cd161e7d9f2c783ee12662b422237eb790ff85b
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2010-01-11 23:22:44 +0000

    Fixed i18n on OS X too (may not work on PPC macs)
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@4433 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/utils/translation.cpp
+++ src/utils/translation.cpp
@@ -38,8 +38,12 @@
 
 #include "io/file_manager.hpp"
 
+// set to 1 to debug i18n
+#define TRANSLATE_VERBOSE 0
+
 
 Translations* translations=NULL;
+bool remove_bom = false;
 
 // ----------------------------------------------------------------------------
 Translations::Translations()
@@ -55,9 +59,20 @@ Translations::Translations()
     setlocale(LC_MESSAGES, "");
 #endif
     bindtextdomain (PACKAGE, file_manager->getTranslationDir().c_str());
-    if (sizeof(wchar_t) == 4) bind_textdomain_codeset(PACKAGE, "UTF-32");
-    else if (sizeof(wchar_t) == 2) bind_textdomain_codeset(PACKAGE, "UTF-16LE");
-    else assert(false);
+    if (sizeof(wchar_t) == 4)
+    {
+        // FIXME: will probably not work on PPC maccs
+        bind_textdomain_codeset(PACKAGE, "UTF-32LE");
+    }
+    else if (sizeof(wchar_t) == 2)
+    {
+        bind_textdomain_codeset(PACKAGE, "UTF-16LE");
+    }
+    else
+    {
+        fprintf(stderr, "Your wchar_t is neither 2 byte-long nor 4. What now??\n");
+        exit(1);
+    }
     
     textdomain (PACKAGE);
 #endif
@@ -68,7 +83,9 @@ const wchar_t* Translations::w_gettext(const char* original)
 {
     if (original[0] == '\0') return L"";
     
+#if TRANSLATE_VERBOSE
     std::cout << "Translating " << original << "\n";
+#endif
     
 #if ENABLE_NLS
     const char* original_t = gettext(original);
@@ -76,13 +93,42 @@ const wchar_t* Translations::w_gettext(const char* original)
     m_converted_string = core::stringw(original);
     return m_converted_string.c_str();
 #endif
+
+    /*
+    std::cout << "--> original_t==original ? " << (original_t==original) << std::endl;
+    int zeros = 0;
+    for (int n=0;; n+=1)
+    {
+        std::cout << original_t[n] << " (" << (unsigned)(original_t[n]) << ")\n";
+        if (original_t[n] == 0)
+        {
+            zeros++;
+            if (zeros >= 4) break;
+        }
+        else
+        {
+            zeros = 0;
+        }
+    }*/
+    
     if(original_t==original)
     {
         m_converted_string = core::stringw(original);
+        
+#if TRANSLATE_VERBOSE
+        std::wcout << L"  translation : " << m_converted_string.c_str() << std::endl;
+#endif
         return m_converted_string.c_str();
     }
+    
+    // print
+    //for (int n=0;; n+=4)
+    
     wchar_t* out_ptr = (wchar_t*)original_t;
-    //std::wcout << L"  translation : " << out_ptr << std::endl;
+    if (remove_bom) out_ptr++;
+#if TRANSLATE_VERBOSE
+    std::wcout << L"  translation : " << out_ptr << std::endl;
+#endif
     return out_ptr;
 
 }
