e92286f24f639885ac379402d26340685d832f8b
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2010-03-27 23:44:05 +0000

    Fixed lists being messed up because some events were denied from them
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@5088 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/guiengine/event_handler.cpp
+++ src/guiengine/event_handler.cpp
@@ -143,21 +143,8 @@ EventPropagation EventHandler::onGUIEvent(const SEvent& event)
                  */
 
             case EGET_ELEMENT_FOCUSED: 
-            {
-                Widget* w = GUIEngine::getWidget(id);
-                if (w == NULL) break;
-                
-                //std::cout << "==== irrlicht widget focused : " << w->m_properties[PROP_ID] << std::endl;
-
-                // forbid list for gaining "irrLicht focus", then they will process key events and
-                // we don't want that since we do our own custom processing for keys
-                if (w->m_type == WTYPE_LIST)
-                {
-                    return EVENT_BLOCK;
-                }
-                                
                 break;
-            }
+                
             case EGET_EDITBOX_ENTER:
             {
                 // currently, enter pressed in text ctrl events can only happen in dialogs.
