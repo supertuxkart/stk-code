32b1300adc8b2af77a3495117b619580e0e80d0c
mbjornstk <mbjornstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2010-04-27 00:59:08 +0000

    Set camera near distance (magic number, needs fixing and documenting why 52 when range is 30) to see inside tunnels with --camera-debug.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@5289 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/graphics/camera.cpp
+++ src/graphics/camera.cpp
@@ -252,6 +252,7 @@ void Camera::update(float dt)
         m_camera->setTarget(xyz);
         xyz.Y = xyz.Y+30;
         m_camera->setPosition(xyz);
+        m_camera->setNearValue(52.0); // To view inside tunnels (FIXME 52>30 why??? makes no sense)
         return;
     }
 
