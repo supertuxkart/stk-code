f2fbe0bcdd4f707abcd64e5d9ee5ce709e91ae74
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2010-04-28 23:29:23 +0000

    Cosmetic change only (have same-named functions declared next to each other).
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@5316 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/utils/string_utils.hpp
+++ src/utils/string_utils.hpp
@@ -75,11 +75,12 @@ namespace StringUtils
         }
     }
 
-    std::string toUpperCase(const std::string&);
-    std::vector<std::string> split(const std::string& s, char c, bool keepSplitChar=false);
-    std::vector<std::string> splitPath(const std::string& path);
-
-    std::vector<irr::core::stringw> split(const irr::core::stringw& s, char c, bool keepSplitChar=false);
+    std::string                     toUpperCase(const std::string&);
+    std::vector<std::string>        split(const std::string& s, char c, 
+                                          bool keepSplitChar=false);
+    std::vector<irr::core::stringw> split(const irr::core::stringw& s, 
+                                          char c, bool keepSplitChar=false);
+    std::vector<std::string>         splitPath(const std::string& path);
     
     // ------------------------------------------------------------------------
     /** Replaces the first %s or %i/%d in the string with the first value 
