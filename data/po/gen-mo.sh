659fb0a86a3f4ee55894e2484dc5b7c8b502eaa7
xapantu <xapantu@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2010-06-28 11:17:31 +0000

    Update translations from Launchpad
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@5579 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- data/po/gen-mo.sh
+++ data/po/gen-mo.sh
@@ -21,4 +21,5 @@ msgfmt uk.po -o uk/LC_MESSAGES/supertuxkart.mo
 msgfmt zh_CN.po -o zh_CN/LC_MESSAGES/supertuxkart.mo
 msgfmt he.po -o he/LC_MESSAGES/supertuxkart.mo
 msgfmt ko.po -o ko/LC_MESSAGES/supertuxkart.mo
+msgfmt sk.po -o sk/LC_MESSAGES/supertuxkart.mo
 
