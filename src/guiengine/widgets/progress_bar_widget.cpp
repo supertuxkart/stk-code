862e5f6310d7a069f3237ccb40b6c61636ebb502
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2010-07-06 22:26:17 +0000

    Minor update to new progress bar control : made non-focusable. Update Xcode project.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@5662 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/guiengine/widgets/progress_bar_widget.cpp
+++ src/guiengine/widgets/progress_bar_widget.cpp
@@ -35,10 +35,10 @@ void ProgressBarWidget::add()
 {
     rect<s32> widget_size = rect<s32>(m_x, m_y, m_x + m_w, m_y + m_h);
     stringw&  message = m_text;
-    m_element = GUIEngine::getGUIEnv()->addButton(widget_size, m_parent, getNewID(), message.c_str(), L"");
+    m_element = GUIEngine::getGUIEnv()->addButton(widget_size, m_parent, getNewNoFocusID(), message.c_str(), L"");
     
     m_id = m_element->getID();
-    m_element->setTabOrder(m_id);
+    m_element->setTabStop(false);
     m_element->setTabGroup(false);
 }
 // -----------------------------------------------------------------------------
