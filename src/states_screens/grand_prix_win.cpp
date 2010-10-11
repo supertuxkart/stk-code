f6cd18f5c7f4d5710f7080aac464c76a5938f3af
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2010-10-11 05:23:59 +0000

    Fixed VS compilation problem.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@6248 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/states_screens/grand_prix_win.cpp
+++ src/states_screens/grand_prix_win.cpp
@@ -421,7 +421,8 @@ void GrandPrixWin::setKarts(const std::string idents_arg[3])
                                                          m_kart_z[n]) );
             kart_main_node->setScale( core::vector3df(0.4f, 0.4f, 0.4f)  );
             kart_model->setAnimation(KartModel::AF_DEFAULT);
-            kart_model->update(0.0f, 0.0f, (float[]){0.0f, 0.0f, 0.0f, 0.0f});
+            float susp[4]={0,0,0,0};
+            kart_model->update(0.0f, 0.0f, susp);
         }
         else
         {
