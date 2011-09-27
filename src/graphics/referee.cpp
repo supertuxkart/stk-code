0a043b57b81ed0bb2e09e97f1b41a675cdabb9a6
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2011-09-27 03:03:43 +0000

    Fixed bugs in the computation of the rotation angle for the referee so that it faces the kart.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@9900 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/graphics/referee.cpp
+++ src/graphics/referee.cpp
@@ -92,10 +92,10 @@ void Referee::init()
     node->get("scale",              &m_st_scale             );
     node->get("start-rotation",     &m_st_start_rotation    );
 
-    float angle_to_kart = atan2(m_st_start_offset.getY(), 
+    float angle_to_kart = atan2(m_st_start_offset.getX(), 
                                 m_st_start_offset.getZ())
                         * RAD_TO_DEGREE;
-    m_st_start_rotation.setY(m_st_start_rotation.getY()-angle_to_kart);
+    m_st_start_rotation.setY(m_st_start_rotation.getY()+angle_to_kart);
 
     std::vector<std::string> colors;
     node->get("colors", &colors);
