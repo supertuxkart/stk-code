941a75410aebdb042a3fa8285fa8cff83848cdf3
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2011-10-07 09:13:44 +0000

    Removed unnecessary include.
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@9956 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/items/bowling.cpp
+++ src/items/bowling.cpp
@@ -17,7 +17,6 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#include "graphics/camera.hpp"
 #include "graphics/material.hpp"
 #include "io/xml_node.hpp"
 #include "items/bowling.hpp"
