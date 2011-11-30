357297dc382e64131c6ff4a5475a583e7ebea8e7
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2011-11-30 23:44:04 +0000

    Very minor bugfix: if a kart got no startup boost, it would nevertheless set a startup boost of 0 in bullet, which causes the first frame not to have correct physics applied (not noticable in game actually).
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@10289 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/karts/kart.cpp
+++ src/karts/kart.cpp
@@ -1541,7 +1541,8 @@ void Kart::updatePhysics(float dt)
     {
         m_has_started = true;
         float f       = m_kart_properties->getStartupBoost();
-        m_vehicle->activateZipper(f);
+        if(f>0)
+            m_vehicle->activateZipper(f);
         MaxSpeed::increaseMaxSpeed(MS_INCREASE_ZIPPER, 0.9f*f,
                                    5.0f, 5.0f);
 
