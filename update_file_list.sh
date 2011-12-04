b37f9865d424acaf5646a367498c23eb3ef17a95
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2011-12-04 23:04:02 +0000

    Change CMake file a little : there is now a scriupt to automagically update the file list by globbing for the list of files
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@10345 178a84e3-b1eb-0310-8ba1-8eac791a3b58

---
+++ update_file_list.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+FILES=`find src -path 'src/bullet' -prune -o -path 'src/ide' -prune -o -regex ".*\.[ch]p*" -exec echo "{} " \;`
+echo $FILES
+sed "s/%LIST_OF_FILES%/$(echo $FILES | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')/g" CMakeLists.in.txt > CMakeLists.txt
