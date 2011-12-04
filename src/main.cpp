cccdbb14f7ffe3d2c0ab30d2d9383883bd738da0
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2011-12-04 01:32:17 +0000

    Remove accidentally committed code
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@10325 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/main.cpp
+++ src/main.cpp
@@ -1148,10 +1148,6 @@ int main(int argc, char *argv[] )
 
         // Get into menu mode initially.
         input_manager->setMode(InputManager::MENU);
-        
-        GUIEngine::addLoadingIcon( irr_driver->getTexture(
-                                                          file_manager->getGUIDir() + "/options_input.png") );
-        
         main_loop = new MainLoop();
         material_manager        -> loadMaterial    ();
         GUIEngine::addLoadingIcon( irr_driver->getTexture(
