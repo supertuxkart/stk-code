939eb6ef3e3319a958244bc25f1d4004672cab16
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2012-01-08 17:04:41 +0000

    Fix compilations regarding stdint.h types
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@10604 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/bullet/src/BulletCollision/BroadphaseCollision/btQuantizedBvh.h
+++ src/bullet/src/BulletCollision/BroadphaseCollision/btQuantizedBvh.h
@@ -41,7 +41,21 @@ class btSerializer;
 #define btQuantizedBvhDataName "btQuantizedBvhFloatData"
 #endif
 
-
+#ifdef __GNUC__
+#include <stdint.h>
+#elif defined(_MSC_VER)
+typedef __int32 int32_t;
+typedef __int64 int64_t;
+typedef __uint16 uint16_t;
+typedef __int8 int8_t;
+typedef unsigned __int32 uint32_t;
+typedef unsigned __int64 uint64_t;
+#else
+typedef int int32_t;
+typedef long long int int64_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long int uint64_t;
+#endif
 
 //http://msdn.microsoft.com/library/default.asp?url=/library/en-us/vclang/html/vclrf__m128.asp
 
