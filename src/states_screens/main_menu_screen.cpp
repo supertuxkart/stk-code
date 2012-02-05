547fd78d6d2c5caa114d13d52bf34db0b50fd31d
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2012-02-05 00:23:31 +0000

    use the kart selected by the player in story mode
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@10801 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/states_screens/main_menu_screen.cpp
+++ src/states_screens/main_menu_screen.cpp
@@ -301,7 +301,13 @@ void MainMenuScreen::eventCallback(Widget* widget, const std::string& name,
         StateManager::get()->createActivePlayer( 
                                                 UserConfigParams::m_all_players.get(0), device );
         
-        race_manager->setLocalKartInfo(0, "tux");
+        if (kart_properties_manager->getKart(UserConfigParams::m_default_kart) == NULL)
+        {
+            fprintf(stderr, "[MainMenuScreen] WARNING: cannot find kart '%s', will revert to default\n",
+                    UserConfigParams::m_default_kart.c_str());
+            UserConfigParams::m_default_kart.revertToDefaults();
+        }
+        race_manager->setLocalKartInfo(0, UserConfigParams::m_default_kart);
         
         // ASSIGN should make sure that only input from assigned devices
         // is read.
