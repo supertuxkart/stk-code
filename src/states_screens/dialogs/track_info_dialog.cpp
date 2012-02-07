c3a9c03b8bff4ec078fefa80ed1c3620aa1177bd
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2012-02-07 05:32:20 +0000

    Worked around the problem that the reverse check box could not be selected by mouse on 800x600 resolution (the checkbox was then outside of the dialog window). A proper solution (imho) should be to convert the dialog into a full-screen menu (which would also be consistent with all other menus).
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/branches/reverse_mode@10823 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/states_screens/dialogs/track_info_dialog.cpp
+++ src/states_screens/dialogs/track_info_dialog.cpp
@@ -151,7 +151,7 @@ TrackInfoDialog::TrackInfoDialog(const std::string& ribbonItem, const std::strin
     if (reverse_available)
     {
         m_checkbox = new CheckBoxWidget();
-        m_checkbox->m_x = m_area.getWidth()/2 - 400;
+        m_checkbox->m_x = 0;
         m_checkbox->m_y = (y2+y3)/2;
         m_checkbox->m_w = 60;
         m_checkbox->m_h = 60;
