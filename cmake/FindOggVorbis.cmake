1ff469d1a68a3908b3cb5dc8a64ad0743fdcea60
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2012-03-06 17:07:16 +0000

    Apply another patch by riebl to move detection of Ogg/Vorbis to the find_package module in a subdir
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@10936 178a84e3-b1eb-0310-8ba1-8eac791a3b58

---
+++ cmake/FindOggVorbis.cmake
@@ -0,0 +1,40 @@
+# - Find OggVorbis
+# Find the OggVorbis includes and libraries
+#
+# Following variables are provided:
+# OGGVORBIS_FOUND
+#     True if OggVorbis has been found
+# OGGVORBIS_INCLUDE_DIRS
+#     The include directories of OggVorbis
+# OGGVORBIS_LIBRARIES
+#     OggVorbis library list
+
+
+find_path(OGGVORBIS_OGG_INCLUDE_DIR NAMES ogg/ogg.h)
+find_path(OGGVORBIS_VORBIS_INCLUDE_DIR NAMES vorbis/vorbisfile.h)
+find_library(OGGVORBIS_OGG_LIBRARY NAMES ogg Ogg)
+find_library(OGGVORBIS_VORBIS_LIBRARY NAMES vorbis Vorbis)
+find_library(OGGVORBIS_VORBISFILE_LIBRARY NAMES vorbisfile)
+
+if (APPLE)
+    set(OGGVORBIS_OGG_INCLUDE_DIR "/Library/Frameworks/Ogg.framework/Headers/")
+    set(OGGVORBIS_VORBIS_INCLUDE_DIR "/Library/Frameworks/Vorbis.framework/Headers/")
+endif()
+
+if(APPLE AND NOT OGGVORBIS_VORBISFILE_LIBRARY)
+    # Seems to be the same on Apple systems
+    set(OGGVORBIS_VORBISFILE_LIBRARY ${OGGVORBIS_VORBIS_LIBRARY})
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(OggVorbis DEFAULT_MSG
+    OGGVORBIS_OGG_INCLUDE_DIR OGGVORBIS_VORBIS_INCLUDE_DIR
+    OGGVORBIS_OGG_LIBRARY OGGVORBIS_VORBIS_LIBRARY OGGVORBIS_VORBISFILE_LIBRARY)
+
+# Publish variables
+set(OGGVORBIS_INCLUDE_DIRS ${OGGVORBIS_OGG_INCLUDE_DIR} ${OGGVORBIS_VORBIS_INCLUDE_DIR})
+set(OGGVORBIS_LIBRARIES ${OGGVORBIS_OGG_LIBRARY} ${OGGVORBIS_VORBIS_LIBRARY} ${OGGVORBIS_VORBISFILE_LIBRARY})
+list(REMOVE_DUPLICATES OGGVORBIS_INCLUDE_DIRS)
+list(REMOVE_DUPLICATES OGGVORBIS_LIBRARIES)
+mark_as_advanced(OGGVORBIS_OGG_INCLUDE_DIR OGGVORBIS_VORBIS_INCLUDE_DIR)
+mark_as_advanced(OGGVORBIS_OGG_LIBRARY OGGVORBIS_VORBIS_LIBRARY OGGVORBIS_VORBISFILE_LIBRARY)
