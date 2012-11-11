82e814dfa4c527e58bb3d2c088de3580e065f095
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2012-11-11 01:30:11 +0000

    Fix mouse cursor not hiding when starting race from overworlD
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@11915 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/states_screens/dialogs/select_challenge.cpp
+++ src/states_screens/dialogs/select_challenge.cpp
@@ -221,6 +221,9 @@ GUIEngine::EventPropagation SelectChallengeDialog::processEvent(const std::strin
         // Sets up kart info, including random list of kart for AI
         network_manager->setupPlayerKartInfo();
         race_manager->startNew(true);
+        
+        irr_driver->hidePointer(); 
+        
         return GUIEngine::EVENT_BLOCK;
     }
     
