591d5d7f707bef3b17453f049996fd8684ed5810
hikerstk <hikerstk@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2012-12-13 02:23:14 +0000

    Use pkgconfig, as recommended by hasufell - thanks for the patch!
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@12275 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- cmake/FindFribidi.cmake
+++ cmake/FindFribidi.cmake
@@ -9,19 +9,27 @@
 # FRIBIDI_LIBRARIES
 #     Fribidi library list
 
-
-find_path(FRIBIDI_INCLUDE_DIR NAMES fribidi/fribidi.h PATHS /Library/Frameworks/fribidi.framework/Headers "${PROJECT_SOURCE_DIR}/dependencies/include")
-find_library(FRIBIDI_LIBRARY NAMES fribidi PATHS /Library/Frameworks/fribidi.framework "${PROJECT_SOURCE_DIR}/dependencies/lib")
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Fribidi DEFAULT_MSG FRIBIDI_INCLUDE_DIR FRIBIDI_LIBRARY)
-
-
-if(APPLE)
-set(FRIBIDI_INCLUDE_DIR "/Library/Frameworks/fribidi.framework/Headers")
+if(UNIX)
+	include(FindPkgConfig)
+	pkg_check_modules(FRIBIDI fribidi)
+else()
+	set(FRIBIDI_FOUND 0)
 endif()
 
-# Publish variables
-set(FRIBIDI_INCLUDE_DIRS ${FRIBIDI_INCLUDE_DIR})
-set(FRIBIDI_LIBRARIES ${FRIBIDI_LIBRARY})
+if(NOT FRIBIDI_FOUND)
+	find_path(FRIBIDI_INCLUDE_DIR NAMES fribidi/fribidi.h PATHS /Library/Frameworks/fribidi.framework/Headers "${PROJECT_SOURCE_DIR}/dependencies/include")
+	find_library(FRIBIDI_LIBRARY NAMES fribidi PATHS /Library/Frameworks/fribidi.framework "${PROJECT_SOURCE_DIR}/dependencies/lib")
+
+	include(FindPackageHandleStandardArgs)
+	find_package_handle_standard_args(Fribidi DEFAULT_MSG FRIBIDI_INCLUDE_DIR FRIBIDI_LIBRARY)
+
+	if(APPLE)
+		set(FRIBIDI_INCLUDE_DIR "/Library/Frameworks/fribidi.framework/Headers")
+	endif()
+
+	# Publish variables
+	set(FRIBIDI_INCLUDE_DIRS ${FRIBIDI_INCLUDE_DIR})
+	set(FRIBIDI_LIBRARIES ${FRIBIDI_LIBRARY})
+endif()
+
 mark_as_advanced(FRIBIDI_INCLUDE_DIR FRIBIDI_LIBRARY)
