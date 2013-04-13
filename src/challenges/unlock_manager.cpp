b7bc1f0ec8095ce9ce0aa2a945793ae0961616e8
wardje <wardje@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2013-04-13 10:44:15 +0000

    Fix compiling in release
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@12643 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/challenges/unlock_manager.cpp
+++ src/challenges/unlock_manager.cpp
@@ -34,6 +34,7 @@
 #include "karts/kart_properties_manager.hpp"
 #include "race/race_manager.hpp"
 #include "tracks/track_manager.hpp"
+#include "utils/log.hpp"
 #include "utils/string_utils.hpp"
 
 UnlockManager* unlock_manager=0;
