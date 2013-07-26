8c6c1d2c4d479a8c7c5b880c51e561b6e1abc8e7
hilnius <hilnius@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2013-07-26 13:02:35 +0000

    adding forgotten files
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/branches/hilnius@13355 178a84e3-b1eb-0310-8ba1-8eac791a3b58

---
+++ src/states_screens/network_kart_selection.cpp
@@ -0,0 +1,11 @@
+#include "network_kart_selection.hpp"
+
+NetworkKartSelectionScreen::NetworkKartSelectionScreen()
+{
+    //ctor
+}
+
+NetworkKartSelectionScreen::~NetworkKartSelectionScreen()
+{
+    //dtor
+}
---
+++ src/states_screens/network_kart_selection.hpp
@@ -0,0 +1,17 @@
+#ifndef NETWORK_KART_SELECTION_HPP
+#define NETWORK_KART_SELECTION_HPP
+
+#include "states_screens/kart_selection.hpp"
+
+
+class NetworkKartSelectionScreen : public KartSelectionScreen
+{
+protected:
+    NetworkKartSelectionScreen();
+    virtual ~NetworkKartSelectionScreen();
+
+public:
+
+};
+
+#endif // NETWORK_KART_SELECTION_HPP
