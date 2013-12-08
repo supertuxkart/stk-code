85191ef9b1e20ba6c5e0ab9c278d73d407d39e1c
auria <auria@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2013-12-08 02:39:38 +0000

    Fix compilation on VS < 2013, tested and confirmed to work by nixt
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@14655 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/utils/types.hpp
+++ src/utils/types.hpp
@@ -26,6 +26,7 @@
       typedef unsigned __int32 uint32_t;
       typedef __int64          int64_t;
       typedef unsigned __int64 uint64_t;
+      typedef char             int8_t;
     #else
     #  include <stdint.h>
     #define SOCKET_ERROR -1
