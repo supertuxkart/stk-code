4c1dd1ada4e6116d627bb7c35f0349ed9b0b6235
samuncle <samuncle@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2013-12-09 02:28:11 +0000

    Minor tweak to change the background of the debug menu
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@14679 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/guiengine/skin.cpp
+++ src/guiengine/skin.cpp
@@ -2194,7 +2194,7 @@ core::recti Skin::draw3DWindowBackground(IGUIElement *element,
 void Skin::draw3DMenuPane (IGUIElement *element, const core::recti &rect,
                            const core::recti *clip)
 {
-    SColor color = SColor(150, 255, 255, 255);
+    SColor color = SColor(150, 44, 34, 90);
     GUIEngine::getDriver()->draw2DRectangle(color, rect);
 }   // draw3DMenuPane
 
