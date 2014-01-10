2aea7a26240ed193a983b17ce16afa426fb8caf5
vincentlj <vincentlj@178a84e3-b1eb-0310-8ba1-8eac791a3b58>
2014-01-10 22:10:21 +0000

    STKMesh: Add some code that just mimic CMeshNode behavior
    git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@14998 178a84e3-b1eb-0310-8ba1-8eac791a3b58

--- src/graphics/stkmesh.cpp
+++ src/graphics/stkmesh.cpp
@@ -1,4 +1,8 @@
 #include "stkmesh.hpp"
+#include "graphics/irr_driver.hpp"
+#include <ISceneManager.h>
+#include <IMaterialRenderer.h>
+#include "glwrap.hpp"
 
 STKMesh::STKMesh(irr::scene::IMesh* mesh, ISceneNode* parent, irr::scene::ISceneManager* mgr,	irr::s32 id,
 	const irr::core::vector3df& position,
@@ -6,10 +10,46 @@ STKMesh::STKMesh(irr::scene::IMesh* mesh, ISceneNode* parent, irr::scene::IScene
 	const irr::core::vector3df& scale) :
 		CMeshSceneNode(mesh, parent, mgr, id, position, rotation, scale)
 {
-
+	for (u32 i=0; i<Mesh->getMeshBufferCount(); ++i)
+	{
+		scene::IMeshBuffer* mb = Mesh->getMeshBuffer(i);
+		if (!mb)
+			continue;
+	}
 }
 
 void STKMesh::render()
 {
-	CMeshSceneNode::render();
+	irr::video::IVideoDriver* driver = irr_driver->getVideoDriver();
+
+	if (!Mesh || !driver)
+		return;
+
+	bool isTransparentPass =
+		SceneManager->getSceneNodeRenderPass() == scene::ESNRP_TRANSPARENT;
+
+	++PassCount;
+
+	driver->setTransform(video::ETS_WORLD, AbsoluteTransformation);
+	Box = Mesh->getBoundingBox();
+
+	for (u32 i=0; i<Mesh->getMeshBufferCount(); ++i)
+	{
+		scene::IMeshBuffer* mb = Mesh->getMeshBuffer(i);
+		if (mb)
+		{
+			const video::SMaterial& material = ReadOnlyMaterials ? mb->getMaterial() : Materials[i];
+
+			video::IMaterialRenderer* rnd = driver->getMaterialRenderer(material.MaterialType);
+			bool transparent = (rnd && rnd->isTransparent());
+
+			// only render transparent buffer if this is the transparent render pass
+			// and solid only in solid pass
+			if (transparent == isTransparentPass)
+			{
+				driver->setMaterial(material);
+				driver->drawMeshBuffer(mb);
+			}
+		}
+	}
 }
