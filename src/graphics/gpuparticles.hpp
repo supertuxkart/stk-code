bff22089478aef83b8d4935de8ac93217bed688a
Vincent Lejeune <vljn@ovi.com>
2014-02-12 17:46:44 +0100

    Remove rain effect.
    Rain enabled map crashes with non glsl codepath, and having a particle
    effect handled separatly from others effect made it unmaintainable.
    Rain must be implemented using gfx file with eventually a custom
    property.

--- src/graphics/gpuparticles.hpp
+++ src/graphics/gpuparticles.hpp
@@ -11,19 +11,6 @@ namespace irr { namespace video{ class ITexture; } }
 
 GLuint getTextureGLuint(irr::video::ITexture *tex);
 
-class GPUParticle : public scene::ISceneNode
-{
-protected:
-    video::SMaterial fakemat;
-    virtual void simulate() = 0;
-    virtual void draw() = 0;
-public:
-    GPUParticle(scene::ISceneNode *parent, scene::ISceneManager* mgr,
-        video::ITexture *tex);
-    virtual void render();
-    virtual void OnRegisterSceneNode();
-};
-
 class ParticleSystemProxy : public scene::CParticleSystemSceneNode {
 protected:
     GLuint tfb_buffers[2], initial_values_buffer, heighmapbuffer, heightmaptexture, quaternionsbuffer;
@@ -73,25 +60,6 @@ public:
     void setIncreaseFactor(float);
     void setHeightmap(const std::vector<std::vector<float> >&, float, float, float, float);
     void setFlip();
-};
-
-class RainNode : public GPUParticle
-{
-protected:
-    GLuint SimulationProgram, RenderProgram, tfb_vertex_buffer[2];
-    unsigned count;
-    GLuint texture, normal_and_depth;
-    GLuint loc_campos, loc_viewm, loc_time;
-    GLuint loc_screenw, loc_screen, loc_invproj, texloc_tex, texloc_normal_and_depths;
-    s32 area;
-    core::aabbox3d<f32> box;
-
-    virtual void simulate();
-    virtual void draw();
-public:
-    RainNode(scene::ISceneManager* mgr, video::ITexture *tex);
-    virtual const core::aabbox3d<f32>& getBoundingBox() const;
-    virtual u32 getMaterialCount() const { return 1; }
 };
 
 #endif // GPUPARTICLES_H
