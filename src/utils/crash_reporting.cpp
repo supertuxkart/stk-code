27a5959fcc2724d366a5e45bcd863734067f2c24
hiker <henrichsjoerg@gmail.com>
2014-04-11 08:00:10 +1000

    Added stack trace to LeakCheck on windows. Re-enabled stack trace printing (though off as default).

--- src/utils/crash_reporting.cpp
+++ src/utils/crash_reporting.cpp
@@ -198,16 +198,24 @@
                 if(last_separator)
                     last_separator[0] = '\0';
 
-                // Finally initialize the symbol handler.
-                BOOL    bOk = _SymInitialize(hProcess, filepath ? filepath : NULL, TRUE);
-                if(!bOk)
-                {
-                    Log::warn("CrashReporting", "SymInitialize() failed");
-                    FreeLibrary(hImageHlpDll);
-                    return;
-                }
+                // Since the stack trace can also be used for leak checks, don't
+                // initialise this all the time.
+                static bool first_time = true;
 
-                _SymSetOptions(SYMOPT_LOAD_LINES);
+                if (first_time)
+                {
+                    // Finally initialize the symbol handler.
+                    BOOL    bOk = _SymInitialize(hProcess, filepath ? filepath : NULL, TRUE);
+                    if (!bOk)
+                    {
+                        Log::warn("CrashReporting", "SymInitialize() failed");
+                        FreeLibrary(hImageHlpDll);
+                        return;
+                    }
+
+                    _SymSetOptions(SYMOPT_LOAD_LINES);
+                    first_time = false;
+                }
             }
 
             // Get the stack trace
