b94fe0fcde49405b208bab19cea9163c6e3200d7
Bart Cools <bcools91@gmail.com>
2014-05-18 14:49:40 +0200

    Profile data now saved without escape characters

--- test.sh
+++ test.sh
@@ -8,7 +8,8 @@ mkdir -p ../batch
 for run in {1..50}; do
     for num in {1..4}; do
 		for laps in {2..7}; do
-			./cmake_build/bin/supertuxkart.app/Contents/MacOS/supertuxkart -R  --mode=3 --numkarts=$num  --with-profile --profile-laps=$laps  --no-graphics | grep "profile" > ../batch/$run.$num.$laps.txt 
+			./cmake_build/bin/supertuxkart.app/Contents/MacOS/supertuxkart -R  --mode=3 --numkarts=$num  --with-profile --profile-laps=$laps  --no-graphics > /dev/null
+			grep "profile" ~/Library/Application\ Support/SuperTuxKart/stdout.log > ../batch/$run.$num.$laps.txt 
 		done
 	done
 done
