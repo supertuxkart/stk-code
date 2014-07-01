b1426dd31d54216752f562703754429e2b221738
Bart Cools <bcools91@gmail.com>
2014-07-01 13:33:35 +0200

    small changes to the tests to get some initial results

--- test.sh
+++ test.sh
@@ -1,47 +1,19 @@
 #!/bin/bash
 
 mkdir -p ../batch
+#tracks='snowmountain city lighthouse olivermath hacienda startrack farm zengarden'
+#karts='gnu tux sara elephpant'
+laps=4
 
-tracks='snowmountain city lighthouse olivermath hacienda startrack farm zengarden'
-karts='beastie sara elephpant tuxley'
-laps=1
-
-for track in $tracks; do
-	#beastie is the current kart
-	#sara is the light kart
-	#tuxley is the medium kart
-	#elephpant is the heavy kart
-	for kart in $karts; do		
-		for run in {1..50}; do
+#for track in $tracks; do
+	#for kart in $karts; do		
+		for run in {901..1500}; do
 			for lap in $laps; do
-				./cmake_build/bin/supertuxkart.app/Contents/MacOS/supertuxkart -R  --mode=3 --numkarts=1  --track=$track --with-profile --profile-laps=$lap --kart=$kart --ai=beastie,beastie,beastie --no-graphics > /dev/null
-				grep "profile" ~/Library/Application\ Support/SuperTuxKart/stdout.log > ../batch/$track.$kart.$run.txt
+				./cmake_build/bin/supertuxkart.app/Contents/MacOS/supertuxkart -R  --mode=3 --numkarts=4  --track=snowmountain --with-profile --profile-laps=4 --kart=gnu --ai=sara,tux,elephpant --no-graphics > /dev/null
+				#./cmake_build/bin/supertuxkart.app/Contents/MacOS/supertuxkart -R  --mode=3 --numkarts=4  --track=$track --with-profile --profile-laps=$lap --kart=$kart --ai=beastie,beastie,beastie --no-graphics > /dev/null
+				#grep "profile" ~/Library/Application\ Support/SuperTuxKart/stdout.log > ../batch/$kart.$track.$run.txt
+				grep "profile" ~/Library/Application\ Support/SuperTuxKart/stdout.log > ../batch/faceoff.$run.txt
 			done
 		done
-	done
-done
-			
-		
-
-
-
-
-#for track in ../stk-assets/tracks/*/track.xml; do	
-#	grep "internal" "$track"
-#	if grep -q "internal" "$track" ; then
-#		continue;
-#	else
-#		if grep -q "arena" "$track" ; then
-#			continue;
-#		else
-#			if grep -q "soccer" "$track" ; then
-#				continue;
-#			else
-#				a=${track%%/track.xml};
-#				trackname=${a##../stk-assets/tracks/};
-#			fi
-#		fi
-#	fi
-#	if [ $trackname = "minel" ] || [ $trackname = "mines" ]; then
-#		continue;
-#	fi
+#	done
+#done
