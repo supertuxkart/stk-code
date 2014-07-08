9220d50035b4f43e9aa4a51a4c7d05e5a5fcce62
nixt <lurk26@gmail.com>
2014-07-09 00:09:48 +0530

    Revert "Correct spatialToTrack and getDistances to work in 3D."
    This reverts commit 5f15138247b270de7810440d3e3e738bf08dd049.

--- src/tracks/graph_node.cpp
+++ src/tracks/graph_node.cpp
@@ -194,13 +194,14 @@ void GraphNode::setDirectionData(unsigned int successor, DirectionType dir,
  */
 void GraphNode::getDistances(const Vec3 &xyz, Vec3 *result)
 {
-
-    // center_line is from A to B, or lower_center to upper_center
-    core::vector3df A = m_lower_center.toIrrVector(), B = m_upper_center.toIrrVector();
-    result->setX(((B - A).crossProduct(xyz.toIrrVector() - A)).getLength() / (B - A).getLength());
-
+    core::vector2df xyz2d(xyz.getX(), xyz.getZ());
+    core::vector2df closest = m_line.getClosestPoint(xyz2d);
+    if(m_line.getPointOrientation(xyz2d)>0)
+        result->setX( (closest-xyz2d).getLength());   // to the right
+    else
+        result->setX(-(closest-xyz2d).getLength());   // to the left
     result->setZ( m_distance_from_start +
-                  (xyz-m_lower_center).length());
+                  (closest-m_lower_center_2d).getLength());
 }   // getDistances
 
 void GraphNode::getDistancesUnrolled(const Vec3 &xyz, const int fork_number, unsigned int quad_idx, Vec3 *result)
@@ -218,7 +219,7 @@ void GraphNode::getDistancesUnrolled(const Vec3 &xyz, const int fork_number, uns
             upper_center = 0.5f*(unrolled[0] + unrolled[1]),
             lower_center = 0.5f*(unrolled[2] + unrolled[3]);
     }
-    
+        // center_line is from A to B, or lower_center to upper_center
     core::vector3df A = lower_center.toIrrVector(), B = upper_center.toIrrVector();
     result->setX(((B - A).crossProduct(xyz.toIrrVector() - A)).getLength() / (B - A).getLength());
 
