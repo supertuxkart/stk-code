edecfbdb5976df2b826ec10ce33bb4ff1eff4c21
Marianne Gagnon <auria.mg@gmail.com>
2014-08-01 19:19:10 -0400

    Ignore minor logging spammed by recent nvidia drivers

--- src/graphics/glwrap.cpp
+++ src/graphics/glwrap.cpp
@@ -96,6 +96,10 @@ CALLBACK
 debugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
               const GLchar* msg, const void *userparam)
 {
+    // ignore minor notifications sent by some drivers (notably the nvidia one)
+    if (severity == GL_DEBUG_SEVERITY_NOTIFICATION)
+        return;
+
     switch(source)
     {
     case GL_DEBUG_SOURCE_API_ARB:
