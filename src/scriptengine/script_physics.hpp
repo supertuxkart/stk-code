f01b69f901b6cc09e6da29ded4f2a621b4c22ac9
Sachith Hasaranga Seneviratne <sachith500@gmail.com>
2014-08-15 21:39:36 +0530

    Formatted script_physics.hpp

--- src/scriptengine/script_physics.hpp
+++ src/scriptengine/script_physics.hpp
@@ -28,34 +28,32 @@ namespace Scripting
     namespace Physics
     {
         
-        //private:
-            //IDs of kart collisions
-            int m_collidingkartid1;
-            int m_collidingkartid2;
+        //IDs of kart collisions
+        int m_collidingkartid1;
+        int m_collidingkartid2;
 
-            //Details of collision
-            std::string m_collider1;
-            std::string m_collider2;
-            std::string m_collisionType;
+        //Details of collision
+        std::string m_collider1;
+        std::string m_collider2;
+        std::string m_collisionType;
         
-        //public:
-            //script engine functions
-            void registerScriptFunctions(asIScriptEngine *engine);
-            asIScriptFunction* 
-                registerScriptCallbacks(asIScriptEngine *engine);
+        //script engine functions
+        void registerScriptFunctions(asIScriptEngine *engine);
+        asIScriptFunction* 
+            registerScriptCallbacks(asIScriptEngine *engine);
 
 
-            //game engine functions
-            void setCollision(int collider1, int collider2);
-            void setCollisionType(std::string);
+        //game engine functions
+        void setCollision(int collider1, int collider2);
+        void setCollisionType(std::string);
 
 
 
-            //script-bound functions
-            void getCollidingKart1(asIScriptGeneric *gen);
-            void getCollidingKart2(asIScriptGeneric *gen);
-            void getCollsionType(asIScriptGeneric *gen);
-            void getCollidingID(asIScriptGeneric *gen);
+        //script-bound functions
+        void getCollidingKart1(asIScriptGeneric *gen);
+        void getCollidingKart2(asIScriptGeneric *gen);
+        void getCollsionType(asIScriptGeneric *gen);
+        void getCollidingID(asIScriptGeneric *gen);
 
     }
     
