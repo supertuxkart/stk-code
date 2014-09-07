28fc15640ce45e03555515219cb5da81e41e366f
Vincent Lejeune <vljn@ovi.com>
2014-09-07 22:57:21 +0200

    Use previous way to detect features

--- src/graphics/irr_driver.cpp
+++ src/graphics/irr_driver.cpp
@@ -506,27 +506,27 @@ void IrrDriver::initDevice()
     // Default false value for hasVSLayer if --no-graphics argument is used
     if (!ProfileWorld::isNoGraphics())
     {
-        if (GLEW_AMD_vertex_shader_layer) {
+        if (hasGLExtension("GL_AMD_vertex_shader_layer")) {
             hasVSLayer = true;
             Log::info("GLDriver", "AMD Vertex Shader Layer enabled");
         }
-        if (GLEW_ARB_buffer_storage) {
+        if (hasGLExtension("GL_ARB_buffer_storage")) {
             hasBuffserStorage = true;
             Log::info("GLDriver", "ARB Buffer Storage enabled");
         }
-        if (GLEW_ARB_base_instance) {
+        if (hasGLExtension("GL_ARB_base_instance")) {
             hasBaseInstance = true;
             Log::info("GLDriver", "ARB Base Instance enabled");
         }
-        if (GLEW_ARB_draw_indirect) {
+        if (hasGLExtension("GL_ARB_draw_indirect")) {
             hasDrawIndirect = true;
             Log::info("GLDriver", "ARB Draw Indirect enabled");
         }
-        if (GLEW_ARB_compute_shader) {
+        if (hasGLExtension("GL_ARB_compute_shader")) {
             hasComputeShaders = true;
             Log::info("GLDriver", "ARB Compute Shader enabled");
         }
-        if (GLEW_ARB_texture_storage) {
+        if (hasGLExtension("GL_ARB_texture_storage")) {
             hasTextureStorage = true;
             Log::info("GLDriver", "ARB Texture Storage enabled");
         }
