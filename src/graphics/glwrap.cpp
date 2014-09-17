377c8e8547655dcf8e26d896ad39473d73d7d00e
konstin <konstin@konstin.no-ip.biz>
2014-09-17 21:43:44 +0200

    removing the return; in the debug callback

--- src/graphics/glwrap.cpp
+++ src/graphics/glwrap.cpp
@@ -32,7 +32,6 @@ CALLBACK
 debugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
               const GLchar* msg, const void *userparam)
 {
-    return;
 #ifdef GL_DEBUG_SEVERITY_NOTIFICATION
     // ignore minor notifications sent by some drivers (notably the nvidia one)
     if (severity == GL_DEBUG_SEVERITY_NOTIFICATION)
