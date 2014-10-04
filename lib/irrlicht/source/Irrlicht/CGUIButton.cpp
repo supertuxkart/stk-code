c1d6c054c8af02c17705f1eb49314c0980685846
Marianne Gagnon <auria.mg@gmail.com>
2014-10-04 19:38:44 -0400

    Route a few more controls rendering through the skin, to make them use OpenGL 3

--- lib/irrlicht/source/Irrlicht/CGUIButton.cpp
+++ lib/irrlicht/source/Irrlicht/CGUIButton.cpp
@@ -277,7 +277,7 @@ void CGUIButton::draw()
 				pos.X += skin->getSize(EGDS_BUTTON_PRESSED_IMAGE_OFFSET_X);
 				pos.Y += skin->getSize(EGDS_BUTTON_PRESSED_IMAGE_OFFSET_Y);
 			}
-			driver->draw2DImage(PressedImage,
+			skin->draw2DImage(PressedImage,
 					ScaleImage? AbsoluteRect :
 						core::recti(pos, PressedImageRect.getSize()),
 					PressedImageRect, &AbsoluteClippingRect,
