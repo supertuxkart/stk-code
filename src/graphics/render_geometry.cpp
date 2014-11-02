1ecd8dcf4f5efd3ff86eebacf2f08ce77e318086
Vincent Lejeune <vljn@ovi.com>
2014-11-02 02:03:06 +0100

    Fix moiré pattern

--- src/graphics/render_geometry.cpp
+++ src/graphics/render_geometry.cpp
@@ -978,6 +978,8 @@ void IrrDriver::renderShadows()
     glDepthMask(GL_TRUE);
     glEnable(GL_DEPTH_TEST);
     glDisable(GL_BLEND);
+    glCullFace(GL_FRONT);
+    glEnable(GL_CULL_FACE);
     m_rtts->getShadowFBO().Bind();
 
     glClearColor(1., 1., 1., 1.);
@@ -1019,6 +1021,9 @@ void IrrDriver::renderShadows()
             renderInstancedShadow<NormalMat>(cascade);
         }
     }
+
+    glCullFace(GL_BACK);
+    glDisable(GL_CULL_FACE);
 
     m_post_processing->renderGaussian6BlurLayer(m_rtts->getShadowFBO());
     glBindTexture(GL_TEXTURE_2D_ARRAY, m_rtts->getShadowFBO().getRTT()[0]);
