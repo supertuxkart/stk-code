6c8548001061b29f6496ba7710c815fef9134cd1
Marianne Gagnon <auria.mg@gmail.com>
2014-11-14 18:09:53 -0500

    Do not use 0,0,0 as default sun position when unspecified, as this will give unpredictable results when normalized

--- src/tracks/track.cpp
+++ src/tracks/track.cpp
@@ -461,7 +461,7 @@ void Track::loadTrackInfo()
     m_default_ambient_color = video::SColor(255, 120, 120, 120);
     m_sun_specular_color    = video::SColor(255, 255, 255, 255);
     m_sun_diffuse_color     = video::SColor(255, 255, 255, 255);
-    m_sun_position          = core::vector3df(0, 0, 0);
+    m_sun_position          = core::vector3df(0, 10, 10);
     irr_driver->setSSAORadius(1.);
     irr_driver->setSSAOK(1.5);
     irr_driver->setSSAOSigma(1.);
