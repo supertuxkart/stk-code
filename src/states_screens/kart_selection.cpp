42d6f0480c8dd049d584199c5bf9fd46bb666ed1
Timothy Hamlett <tjh.hts@gmail.com>
2014-12-01 21:48:12 -0600

    Actually fix #1754
    Thanks to Hiker for the help!

--- src/states_screens/kart_selection.cpp
+++ src/states_screens/kart_selection.cpp
@@ -854,7 +854,7 @@ void KartSelectionScreen::updateKartWidgetModel(uint8_t widget_id,
         else
         {
             m_kart_widgets[widget_id].m_kart_name
-            ->setText(_("Solve challenges to unlock"), false );
+            ->setText(_("Locked : solve active challenges to gain access to more!"), false );
         }
     }
     else
@@ -1445,7 +1445,7 @@ void KartSelectionScreen::setKartsFromCurrentGroup()
         const KartProperties* prop = karts.get(i);
         if (PlayerManager::getCurrentPlayer()->isLocked(prop->getIdent()))
         {
-            w->addItem(_("Solve challenges to unlock"),
+            w->addItem(_("Locked : solve active challenges to gain access to more!"),
                        ID_LOCKED + prop->getIdent(),
                        prop->getAbsoluteIconFile(), LOCKED_BADGE,
                        IconButtonWidget::ICON_PATH_TYPE_ABSOLUTE);
