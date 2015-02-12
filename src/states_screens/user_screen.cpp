f437781dcceea8f68b1ee7d0bd5aa921f5fdd097
hiker <henrichsjoerg@gmail.com>
2015-02-13 08:41:19 +1100

    Fix #1980.

--- src/states_screens/user_screen.cpp
+++ src/states_screens/user_screen.cpp
@@ -360,6 +360,7 @@ void BaseUserScreen::eventCallback(Widget* widget,
             PlayerProfile *cp = getSelectedPlayer();
             RegisterScreen::getInstance()->setRename(cp);
             RegisterScreen::getInstance()->push();
+            RegisterScreen::getInstance()->setParent(this);
             m_new_registered_data = false;
             m_auto_login          = false;
             // Init will automatically be called, which
