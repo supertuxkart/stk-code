c8909b91975bb19d2f51e974214d91d28ab01025
hiker <henrichsjoerg@gmail.com>
2015-07-26 00:30:09 +1000

    Fixed missing display of points for non-winners in a FTL GP (partly fix for #2249).

--- src/states_screens/race_result_gui.cpp
+++ src/states_screens/race_result_gui.cpp
@@ -842,7 +842,8 @@ void RaceResultGUI::determineGPLayout()
         ri->m_y_pos          = (float)(m_top+rank*m_distance_between_rows);
         int p                = race_manager->getKartPrevScore(kart_id);
         ri->m_current_displayed_points = (float)p;
-        if (kart->isEliminated())
+        if (kart->isEliminated() && 
+            race_manager->getMinorMode()!=RaceManager::MINOR_MODE_FOLLOW_LEADER)
         {
             ri->m_new_points = 0;
         }
