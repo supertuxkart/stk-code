supertuxkart/stk-code

commit 9b562d31f4e63e3411155fee01aa8ca5baa6a3fb

Replaced Widget's set(De)Activated functions with one setActive(bool) function, which simplifies code for various screen a lot.

src/states_screens/options_screen_video.cpp
@@ -192,9 +192,7 @@ void OptionsScreenVideo::init()
     CheckBoxWidget* rememberWinpos = getWidget<CheckBoxWidget>("rememberWinpos");
     rememberWinpos->setState(UserConfigParams::m_remember_window_location);
 
-    if (UserConfigParams::m_fullscreen) rememberWinpos->setDeactivated();
-    else rememberWinpos->setActivated();
-
+    rememberWinpos->setActive(UserConfigParams::m_fullscreen);
 
     // --- get resolution list from irrlicht the first time
     if (!m_inited)
@@ -353,23 +351,13 @@ void OptionsScreenVideo::init()
     // ---- forbid changing resolution or animation settings from in-game
     // (we need to disable them last because some items can't be edited when
     // disabled)
-    if (StateManager::get()->getGameState() == GUIEngine::INGAME_MENU)
-    {
-        res->setDeactivated();
-        full->setDeactivated();
-        applyBtn->setDeactivated();
-        gfx->setDeactivated();
-        getWidget<ButtonWidget>("custom")->setDeactivated();
-    }
-    else
-    {
-        // Enable back widgets if they were visited in-game previously
-        res->setActivated();
-        full->setActivated();
-        applyBtn->setActivated();
-        gfx->setActivated();
-        getWidget<ButtonWidget>("custom")->setActivated();
-    }
+    bool in_game = StateManager::get()->getGameState() == GUIEngine::INGAME_MENU;
+
+    res->setActive(!in_game);
+    full->setActive(!in_game);
+    applyBtn->setActive(!in_game);
+    gfx->setActive(!in_game);
+    getWidget<ButtonWidget>("custom")->setActive(!in_game);
 }   // init
 
 // ----------------------------------------------------------------------------
@@ -597,8 +585,7 @@ void OptionsScreenVideo::eventCallback(Widget* widget, const std::string& name,
         CheckBoxWidget* fullscreen = getWidget<CheckBoxWidget>("fullscreen");
         CheckBoxWidget* rememberWinpos = getWidget<CheckBoxWidget>("rememberWinpos");
 
-        if (fullscreen->getState()) rememberWinpos->setDeactivated();
-        else rememberWinpos->setActivated();
+        rememberWinpos->setActive(!fullscreen->getState());
     }
 }   // eventCallback
 

src/guiengine/widget.cpp
@@ -148,29 +148,15 @@ void Widget::elementRemoved()
 
 // -----------------------------------------------------------------------------
 
-void Widget::setActivated()
+void Widget::setActive(bool active)
 {
     // even if this one is already active, do it anyway on purpose, maybe the
     // children widgets need to be updated
-    m_deactivated = false;
+    m_deactivated = !active;
     const int count = m_children.size();
     for (int n=0; n<count; n++)
     {
-        m_children[n].setActivated();
-    }
-}
-
-// -----------------------------------------------------------------------------
-
-void Widget::setDeactivated()
-{
-    // even if this one is already inactive, do it anyway on purpose, maybe the
-    // children widgets need to be updated
-    m_deactivated = true;
-    const int count = m_children.size();
-    for (int n=0; n<count; n++)
-    {
-        m_children[n].setDeactivated();
+        m_children[n].setActive(active);
     }
 }
 

src/guiengine/widget.hpp
@@ -322,7 +322,7 @@ namespace GUIEngine
         /**
          * \brief Sets the widget (and its children, if any) visible or not.
          * Note that setting a widget invisible implicitely calls setDeactivated(), and setting
-         * it visible implicitely calls setActivated(). If you mix visiblity and (de)activated calls,
+         * it visible implicitely calls setActive(true). If you mix visiblity and (de)activated calls,
          * undefined behavior may ensue (like invisible but clickable buttons).
          */
         void setVisible(bool visible);
@@ -352,12 +352,11 @@ namespace GUIEngine
          * \{
          */
 
-        /** \brief undos setDeactivated() */
-        virtual void setActivated();
-
-        /** \brief greys out the widget, making it not clickable for the user */
-        virtual void setDeactivated();
-
+        /** \brief Sets an widget to be either activated or deactivated 
+         *  (i.e. greyed out)
+         *  \param active Active (true) or deactive (false). Defaults to 
+         *         true. */
+        virtual void setActive(bool active=true);
 
         /**
           * \}

src/guiengine/widgets/spinner_widget.cpp
@@ -395,36 +395,34 @@ void SpinnerWidget::setValue(irr::core::stringw new_value)
 
 // -----------------------------------------------------------------------------
 
-void SpinnerWidget::setActivated()
+void SpinnerWidget::setActive(bool active)
 {
-    Widget::setActivated();
+    Widget::setActive(active);
 
-    setText(L"");
-    if (m_customText.empty())
+    if (active)
     {
-        setValue( getValue() ); // Update the display
+        setText(L"");
+        if (m_customText.empty())
+        {
+            setValue(getValue()); // Update the display
+        }
+        else
+        {
+            setCustomText(m_customText);
+        }
     }
     else
     {
-        setCustomText(m_customText);
+        // Save it temporary because setValue(which is uses for update in
+        // this case) overwrites it
+        core::stringw customText = m_customText;
+        setText(L"-");
+        setValue(getValue()); // Update the display
+        m_customText = customText;
     }
-}
+}   // setActive
 
 // -----------------------------------------------------------------------------
-
-void SpinnerWidget::setDeactivated()
-{
-    Widget::setDeactivated();
-
-    // Save it temporary because setValue(which is uses for update in this case) overwrites it
-    core::stringw customText = m_customText; 
-    setText(L"-");
-    setValue( getValue() ); // Update the display
-    m_customText = customText;
-}
-
-// -----------------------------------------------------------------------------
-
 void SpinnerWidget::setCustomText(const core::stringw& text)
 {
     m_customText = text;

src/guiengine/widgets/spinner_widget.hpp
@@ -193,10 +193,7 @@ namespace GUIEngine
         
         // --------------------------------------------------------------------
         /** Override method from base class Widget */
-        virtual void setActivated();
-
-        /** Override method from base class Widget */
-        virtual void setDeactivated();
+        virtual void setActive(bool active = true);
 
         /** Display custom text in spinner */
         void setCustomText(const core::stringw& text);

src/guiengine/widgets/text_box_widget.cpp
@@ -174,30 +174,16 @@ void TextBoxWidget::elementRemoved()
 
 // -----------------------------------------------------------------------------
 
-void TextBoxWidget::setActivated()
+void TextBoxWidget::setActive(bool active)
 {
-    Widget::setActivated();
+    Widget::setActive(active);
 
     if (m_element != NULL)
     {
         IGUIEditBox* textCtrl = Widget::getIrrlichtElement<IGUIEditBox>();
         assert(textCtrl != NULL);
-        textCtrl->setEnabled(true);
+        textCtrl->setEnabled(active);
     }
-}
-
-// -----------------------------------------------------------------------------
-
-void TextBoxWidget::setDeactivated()
-{
-    Widget::setDeactivated();
-
-    if (m_element != NULL)
-    {
-        IGUIEditBox* textCtrl = Widget::getIrrlichtElement<IGUIEditBox>();
-        assert(textCtrl != NULL);
-        textCtrl->setEnabled(false);
-    }
-}
+}   // setActive
 
 // -----------------------------------------------------------------------------

src/guiengine/widgets/text_box_widget.hpp
@@ -73,10 +73,7 @@ namespace GUIEngine
         virtual void elementRemoved();
 
         /** Override method from base class Widget */
-        virtual void setActivated();
-
-        /** Override method from base class Widget */
-        virtual void setDeactivated();
+        virtual void setActive(bool active=true);
     };
 }
 

src/states_screens/addons_screen.cpp
@@ -143,7 +143,7 @@ void AddonsScreen::init()
     m_sort_default = true;
     m_sort_col = 0;
 
-    getWidget<GUIEngine::RibbonWidget>("category")->setDeactivated();
+    getWidget<GUIEngine::RibbonWidget>("category")->setActive(false);
 
     if(UserConfigParams::logAddons())
         Log::info("addons", "Using directory <%s>", file_manager->getAddonsDir().c_str());
@@ -157,10 +157,8 @@ void AddonsScreen::init()
     w_list->setIcons(m_icon_bank, (int)(m_icon_height));
 
     m_type = "kart";
-    if (UserConfigParams::m_internet_status != RequestManager::IPERM_ALLOWED)
-        getWidget<GUIEngine::IconButtonWidget>("reload")->setDeactivated();
-    else
-        getWidget<GUIEngine::IconButtonWidget>("reload")->setActivated();
+    bool ip = UserConfigParams::m_internet_status == RequestManager::IPERM_ALLOWED;
+    getWidget<GUIEngine::IconButtonWidget>("reload")->setActive(ip);
 
     // Reset filter.
     GUIEngine::TextBoxWidget* w_filter_name =
@@ -371,7 +369,7 @@ void AddonsScreen::loadList()
         }
     }
 
-    getWidget<GUIEngine::RibbonWidget>("category")->setActivated();
+    getWidget<GUIEngine::RibbonWidget>("category")->setActive(true);
     if(m_type == "kart")
         getWidget<GUIEngine::RibbonWidget>("category")->select("tab_kart",
                                                         PLAYER_ID_GAME_MASTER);

commit 07b0ab0d78953bb59d6a613bb54d46506095c293

Fixed disabling "remember window pos" button when fullscreen is enabled.
Regression since commit 9b562d31f4e63e3411155fee01aa8ca5baa6a3fb

src/states_screens/options_screen_video.cpp
@@ -200,7 +200,7 @@ void OptionsScreenVideo::init()
     CheckBoxWidget* rememberWinpos = getWidget<CheckBoxWidget>("rememberWinpos");
     rememberWinpos->setState(UserConfigParams::m_remember_window_location);
 
-    rememberWinpos->setActive(UserConfigParams::m_fullscreen);
+    rememberWinpos->setActive(!UserConfigParams::m_fullscreen);
 
     // --- get resolution list from irrlicht the first time
     if (!m_inited)