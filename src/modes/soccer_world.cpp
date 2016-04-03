740e4e10c8a99bf84c76de9db464b7b5474f6983
Benau <Benau@users.noreply.github.com>
2016-04-04 07:53:33 +0800

    Remove soccer team color handling in kart model
    Let hope in the future someone is smart enough to implement that...

--- src/modes/soccer_world.cpp
+++ src/modes/soccer_world.cpp
@@ -398,8 +398,9 @@ AbstractKart *SoccerWorld::createKart(const std::string &kart_ident, int index,
     m_kart_position_map[index] = (unsigned)(pos_index - 1);
 
     AbstractKart *new_kart = new Kart(kart_ident, index, position, init_pos,
-            difficulty, team == SOCCER_TEAM_BLUE ?
-            video::ERT_BLUE : video::ERT_RED);
+              difficulty);
+            //difficulty, team == SOCCER_TEAM_BLUE ?
+            //video::ERT_BLUE : video::ERT_RED);
     new_kart->init(race_manager->getKartType(index));
     Controller *controller = NULL;
 
