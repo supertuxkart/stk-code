17867219a0262604c0198e271a67800949d211d5
Deve <deveee@gmail.com>
2016-05-20 19:55:39 +0200

    Added a check for GL_ARB_explicit_attrib_location extension.
    It is enabled by default for OpenGL 3.3, but we need it for some of our shaders in render_geometry.cpp (which are OpenGL >= 3.2) and also for InstancedColorizeShader.

--- src/graphics/graphics_restrictions.cpp
+++ src/graphics/graphics_restrictions.cpp
@@ -56,6 +56,7 @@ namespace GraphicsRestrictions
             "BindlessTexture",
             "TextureCompressionS3TC",
             "AMDVertexShaderLayer",
+            "ExplicitAttribLocation",
             "DriverRecentEnough",
             "HighDefinitionTextures",
             "AdvancedPipeline",
