40697ab5f7304c3bda260e73ea0722f8bf07e73f
hiker <henrichsjoerg@gmail.com>
2016-08-05 17:22:04 +1000

    Bugfix for rewind: previously for a floating point value f add(f) would actually call add(Vec3(f)), which adds a vector (f,f,f).

--- src/network/network_string.hpp
+++ src/network/network_string.hpp
@@ -228,6 +228,12 @@ public:
     }   // operator+=
 
     // ------------------------------------------------------------------------
+    /** Adds a floating point number */
+    BareNetworkString& add(float f)
+    {
+        return addFloat(f);
+    }   // add
+        // ------------------------------------------------------------------------
     /** Adds the xyz components of a Vec3 to the string. */
     BareNetworkString& add(const Vec3 &xyz)
     {
