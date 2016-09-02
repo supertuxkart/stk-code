971b5a81e7b41036dc5ff248657e3861181a4fc5
Suici Doga <suiciwd@gmail.com>
2016-09-02 10:01:40 +0530

    Add optimize shaders script

---
+++ tools/optimize_shaders.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+echo Copying data/shaders to data/shaders_optimized
+cp -R data/shaders data/shaders_optimized # Copy shaders to data/shaders_optimized
+
+cd data/shaders_optimized
+
+find . -type f -name "*.vert" -exec sed -i '1i  #version 140' {} \;
+find . -type f -name "*.vert" -exec glslopt -v {} {} \;
+find . -type f -name "*.vert" -exec sed -i '/#version 140/d' {} \;
+
+echo "version #140" >> ./pass.vert # To fix the crash
+
