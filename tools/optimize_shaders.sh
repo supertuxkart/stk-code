6d6598083511e6940b1dacd53058d5738b0fedda
Suici Doga <suiciwd@gmail.com>
2016-09-03 16:08:19 +0530

    Fix file paths

--- tools/optimize_shaders.sh
+++ tools/optimize_shaders.sh
@@ -5,9 +5,9 @@ cp -R data/shaders data/shaders_optimized # Copy shaders to data/shaders_optimiz
 
 cd data/shaders_optimized
 
-find . -type f -name "*.vert" -exec /mnt/storage/princ/Documents/Programming/stk-code/tools/optimize_glsl.sh {} -v $1 \;
+find . -type f -name "*.vert" -exec ../../tools/optimize_glsl.sh {} -v $1 \;
 
-find . -type f -name "*.frag" -exec /mnt/storage/princ/Documents/Programming/stk-code/tools/optimize_glsl.sh {} -f $1 \;
+find . -type f -name "*.frag" -exec ../../tools/optimize_glsl.sh {} -f $1 \;
 
 # Use unoptimized version of faulty shaders
 cp ../shaders/particlesimheightmap.vert .
@@ -15,4 +15,4 @@ cp ../shaders/texturedquad.vert .
 cp ../shaders/pointemitter.vert .
 cp ../shaders/importance_sampling_specular.frag .
 cp ../shaders/screenquad.vert .
-cp ../shaders/colortexturequad.vert .
+cp ../shaders/colortexturedquad.vert .
