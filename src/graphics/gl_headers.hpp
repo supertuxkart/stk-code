3add543a221d512c7c6239ae769e088fc9203d9e
Deve <deveee@gmail.com>
2016-11-30 21:22:34 +0100

    Fixed conflict between GLES2 headers included in irrlicht and GLES3 headers included in STK.
    There is one minor difference in one declaration, which is probably an inconsistency in these headers. But it may cause compilation errors on some compiler versions.

--- src/graphics/gl_headers.hpp
+++ src/graphics/gl_headers.hpp
@@ -41,6 +41,7 @@ extern "C" {
 #        define GL_TEXTURE_SWIZZLE_RGBA 0x8E46
 #    endif
 #elif defined(USE_GLES2)
+#    define __gl2_h_
 #    include <GLES3/gl3.h>
 #    include <GLES3/gl3ext.h>
 #    include <GLES2/gl2ext.h>
