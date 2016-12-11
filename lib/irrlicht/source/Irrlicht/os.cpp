497ba8d89a20108d0526db2bd9cc972155364b55
Daniel Ölschlegel <amoibos@gmail.com>
2016-12-11 22:07:34 +0100

    Add a little more BSD detection
    Tested for NetBSD, the same signature has DragonFly(http://fxr.watson.org/fxr/source/sys/endian.h?v=DFBSD#L42)

--- lib/irrlicht/source/Irrlicht/os.cpp
+++ lib/irrlicht/source/Irrlicht/os.cpp
@@ -22,7 +22,7 @@
 	#include <libkern/OSByteOrder.h>
 	#define bswap_16(X) OSReadSwapInt16(&X,0)
 	#define bswap_32(X) OSReadSwapInt32(&X,0)
-#elif defined(__FreeBSD__)
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
 	#include <sys/endian.h>
 	#define bswap_16(X) bswap16(X)
 	#define bswap_32(X) bswap32(X)
