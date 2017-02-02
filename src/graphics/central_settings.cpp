2557a700cc9e9c05299234acb2240aad43c26cca
Deve <deveee@gmail.com>
2017-02-02 22:26:37 +0100

    Just to be more clear about explicit attrib location availability in GLES

--- src/graphics/central_settings.cpp
+++ src/graphics/central_settings.cpp
@@ -239,7 +239,8 @@ void CentralVideoSettings::init()
             hasTextureSwizzle = true;
         }
         
-        if (!GraphicsRestrictions::isDisabled(GraphicsRestrictions::GR_EXPLICIT_ATTRIB_LOCATION))
+        if (!GraphicsRestrictions::isDisabled(GraphicsRestrictions::GR_EXPLICIT_ATTRIB_LOCATION) &&
+            m_glsl == true)
         {
             Log::info("GLDriver", "Explicit Attrib Location Present");
             hasExplicitAttribLocation = true;
