d339d9acefb4086a1e3c9e3cc6081b1dd2c07e9e
Deve <deveee@gmail.com>
2017-03-08 21:41:53 +0100

    Fixed ambient light for legacy pipeline.
    TODO: It should be possible to convert it once together with spherical harmonics computations.

--- src/graphics/irr_driver.cpp
+++ src/graphics/irr_driver.cpp
@@ -1653,7 +1653,12 @@ void IrrDriver::onUnloadWorld()
 void IrrDriver::setAmbientLight(const video::SColorf &light, bool force_SH_computation)
 {
 #ifndef SERVER_ONLY
-    m_scene_manager->setAmbientLight(light);
+    video::SColorf color = light;
+    color.r = powf(color.r, 1.0f / 2.2f);
+    color.g = powf(color.g, 1.0f / 2.2f);
+    color.b = powf(color.b, 1.0f / 2.2f);
+    
+    m_scene_manager->setAmbientLight(color);
     m_renderer->setAmbientLight(light, force_SH_computation);    
 #endif
 }   // setAmbientLight
