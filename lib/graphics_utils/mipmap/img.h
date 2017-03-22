aecb1a97ce361a3976ff682dd8285640435abff9
Benau <Benau@users.noreply.github.com>
2017-03-23 00:12:06 +0800

    Update license of img.h as Stragus said in IRC
    Also remove unneeded file

--- lib/graphics_utils/mipmap/img.h
+++ lib/graphics_utils/mipmap/img.h
@@ -1,22 +1,25 @@
-/* *****************************************************************************
+/* -----------------------------------------------------------------------------
  *
- * Copyright (c) 2007-2016 Alexis Naveros.
+ * Copyright (c) 2007-2017 Alexis Naveros.
  * Portions developed under contract to the SURVICE Engineering Company.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * version 2.1 as published by the Free Software Foundation.
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
  *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this file; see the file named COPYING for more
- * information.
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
  *
- * *****************************************************************************
+ * -----------------------------------------------------------------------------
  */
 
 #ifndef IMG_H
