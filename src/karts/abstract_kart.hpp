99269f3411b0422a40a48b0708c531a1c882f103
Alayan-stk-2 <Alayan-stk-2@users.noreply.github.com>
2018-03-17 13:45:19 +0100

    Remember last used powerup for AI

--- src/karts/abstract_kart.hpp
+++ src/karts/abstract_kart.hpp
@@ -389,6 +389,9 @@ public:
     /** Returns the current powerup. */
     virtual const Powerup *getPowerup() const = 0;
     // ------------------------------------------------------------------------
+    /** Returns the last used powerup. */
+    virtual const Powerup *getLastUsedPowerup() const = 0;
+    // ------------------------------------------------------------------------
     /** Returns the current powerup. */
     virtual Powerup *getPowerup() = 0;
     // ------------------------------------------------------------------------
