c488c63d38e99b0d10732979bb57940a5efec370
auria.mg <auria.mg@gmail.com>
2018-03-31 21:16:25 -0400

    Rollback part of the recently merged PR that did not belong with AI changes

--- src/karts/kart.cpp
+++ src/karts/kart.cpp
@@ -2460,9 +2460,6 @@ void Kart::updateEnginePowerAndBrakes(float dt)
     updateNitro(dt);
     float engine_power = getActualWheelForce();
 
-    // apply nitro boost if relevant
-    if(getSpeedIncreaseTimeLeft(MaxSpeed::MS_INCREASE_NITRO))
-        engine_power*=1.2f;
     // apply parachute physics if relevant
     if(m_attachment->getType()==Attachment::ATTACH_PARACHUTE)
         engine_power*=0.2f;
