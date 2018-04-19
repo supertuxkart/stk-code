dd1b2563651956932a5d4e627a0170c51416139a
Alayan-stk-2 <Alayan-stk-2@users.noreply.github.com>
2018-04-19 06:05:25 +0200

    Add support for SuperTux challenges

--- src/states_screens/race_gui_overworld.hpp
+++ src/states_screens/race_gui_overworld.hpp
@@ -65,10 +65,11 @@ private:
     video::ITexture *m_trophy1;
     video::ITexture *m_trophy2;
     video::ITexture *m_trophy3;
+    video::ITexture *m_trophy4;
     video::ITexture *m_lock;
     video::ITexture *m_open_challenge;
 
-    video::ITexture* m_icons[5];
+    video::ITexture* m_icons[6];
 
     /** The size of a single marker on the screen for AI karts,
      *  need not be a power of 2. */
