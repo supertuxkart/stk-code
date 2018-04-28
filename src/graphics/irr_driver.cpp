49ff8ecd25b3f6b5569e2930ac675e4f121b8b4b
Alayan-stk-2 <Alayan-stk-2@users.noreply.github.com>
2018-04-28 23:06:25 +0200

    Reload powerup models on graphics options restart

--- src/graphics/irr_driver.cpp
+++ src/graphics/irr_driver.cpp
@@ -942,6 +942,7 @@ void IrrDriver::applyResolutionSettings()
         material_manager->addSharedMaterial(materials_file);
     }
 
+    powerup_manager->loadPowerupsModels();
     ItemManager::loadDefaultItemMeshes();
     projectile_manager->loadData();
     Referee::init();
