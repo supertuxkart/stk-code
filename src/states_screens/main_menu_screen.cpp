e1a1196a9f8f7ea8b1ba7101581f7077bafeddaf
auria.mg <auria.mg@gmail.com>
2018-05-02 19:35:04 -0400

    jgh

--- src/states_screens/main_menu_screen.cpp
+++ src/states_screens/main_menu_screen.cpp
@@ -15,7 +15,7 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#define DEBUG_MENU_ITEM 0
+#define DEBUG_MENU_ITEM 1
 
 #include "states_screens/main_menu_screen.hpp"
 
