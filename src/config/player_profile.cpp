7d4cef636eb5512ca317b420fc65d270d6f6f999
Alayan-stk-2 <Alayan-stk-2@users.noreply.github.com>
2018-05-19 16:16:11 +0200

    Fix another bug when there is no current player yet

--- src/config/player_profile.cpp
+++ src/config/player_profile.cpp
@@ -216,8 +216,14 @@ void PlayerProfile::save(UTFWriter &out)
     out << L"            remember-password=\""         << m_remember_password << L"\"\n";
     out << L"            default-kart-color=\""        << m_default_kart_color << L"\">\n";
     {
+        bool is_current_player = false;
+        PlayerProfile *player = PlayerManager::getCurrentPlayer();
+
+        if (player != NULL && (getName() == player->getName()));
+            is_current_player = true;
+
         if(m_story_mode_status)
-            m_story_mode_status->save(out);
+            m_story_mode_status->save(out, is_current_player);
 
         if(m_achievements_status)
             m_achievements_status->save(out);
@@ -278,4 +284,3 @@ bool PlayerProfile::operator<(const PlayerProfile &other)
 }   // operator<
 
 // -----------------------------------------------------------------------------
-
