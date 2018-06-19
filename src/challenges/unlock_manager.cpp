ed8432a949c67962a09addbf5da5eb94f2ecd68d
Alayan-stk-2 <Alayan-stk-2@users.noreply.github.com>
2018-06-20 01:45:12 +0200

    Remove prints

--- src/challenges/unlock_manager.cpp
+++ src/challenges/unlock_manager.cpp
@@ -253,31 +253,19 @@ void UnlockManager::findWhatWasUnlocked(int points_before, int points_now,
                                         std::vector<std::string>& karts,
                                         std::vector<const ChallengeData*>& unlocked)
 {
-    printf ("points before is %d, points_now is %d\n", points_before, points_now);
+    ChallengeData* c = NULL;
 
     for (AllChallengesType::iterator it = m_all_challenges.begin();
          it != m_all_challenges.end(); it++)
-    {     
-        ChallengeData* c = it->second;
-
+    {
+        c = it->second;
         if (c->getNumTrophies() > points_before &&
             c->getNumTrophies() <= points_now      )
         {
-            printf ("Num trophies for a challenge between before and now !)\n");
-            if (c->getMode() != ChallengeData::CM_SINGLE_RACE)
-                printf ("not single race major mode !\n");
-
-            if (c->getTrackId() == "")
-                printf ("empty track ID !\n");
-
             if (c->getMode() == ChallengeData::CM_SINGLE_RACE && c->getTrackId() != "")
             {
-                printf("We've verified the mode status\n");
                 if (!PlayerManager::getCurrentPlayer()->isLocked(c->getTrackId()))
-                {
                     tracks.push_back(c->getTrackId());
-                    printf("track pushed back to list of unlocked feature\n");
-                }
             }
             else if (c->getMode() == ChallengeData::CM_GRAND_PRIX && c->getGPId() != "")
             {
