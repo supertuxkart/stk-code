dc43e3d5b23105369c4dcfe09fb72cab788b3412
Alayan-stk-2 <Alayan-stk-2@users.noreply.github.com>
2018-08-21 02:52:10 +0200

    Assign kart type to karts not having a valid one (#3385)
    * Fix #3375
    
    * Remove debug print
    
    * Don't clear the list of kart types when changing resolution

--- src/karts/kart_properties_manager.cpp
+++ src/karts/kart_properties_manager.cpp
@@ -215,6 +215,7 @@ void KartPropertiesManager::loadCharacteristics(const XMLNode *root)
     for (const XMLNode *type : nodes)
     {
         type->get("name", &name);
+        m_kart_types.push_back(name);
         m_kart_type_characteristics.insert(std::pair<const std::string,
             std::unique_ptr<AbstractCharacteristic> >(name,
             std::unique_ptr<AbstractCharacteristic>(new XmlCharacteristic(type))));
@@ -303,10 +304,24 @@ const AbstractCharacteristic* KartPropertiesManager::getDifficultyCharacteristic
 }   // getDifficultyCharacteristic
 
 //-----------------------------------------------------------------------------
-const AbstractCharacteristic* KartPropertiesManager::getKartTypeCharacteristic(const std::string &type) const
+const AbstractCharacteristic* KartPropertiesManager::getKartTypeCharacteristic(const std::string &type,
+                                                                               const std::string &name) const
 {
+    bool type_is_valid = false;
+    for (unsigned i=0; i < m_kart_types.size(); i++)
+    {
+        if (type == m_kart_types[i])
+            type_is_valid = true;
+    }
+
+    if (!type_is_valid)
+        Log::warn("KartProperties", "Can't find kart type '%s' for kart '%s', defaulting to '%s'.",
+            type.c_str(), name.c_str(), m_kart_types[0].c_str());
+
+    std::string valid_type = (type_is_valid) ? type : m_kart_types[0];
+
     std::map<std::string, std::unique_ptr<AbstractCharacteristic> >::const_iterator
-        it = m_kart_type_characteristics.find(type);
+        it = m_kart_type_characteristics.find(valid_type);
     if (it == m_kart_type_characteristics.cend())
         return nullptr;
     return it->second.get();
