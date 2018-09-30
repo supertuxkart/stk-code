a2d4763201b04fbc8d104df67e238c05a8639b91
Benau <Benau@users.noreply.github.com>
2018-09-30 16:30:27 +0800

    Hide minimap when hide all for artist debug mode

--- src/states_screens/race_gui.cpp
+++ src/states_screens/race_gui.cpp
@@ -261,12 +261,13 @@ void RaceGUI::renderGlobal(float dt)
     if(world->getPhase() == World::GOAL_PHASE)
         drawGlobalGoal();
 
+    if (!m_enabled) return;
+
     // MiniMap is drawn when the players wait for the start countdown to end
     drawGlobalMiniMap();
-    
+
     // Timer etc. are not displayed unless the game is actually started.
     if(!world->isRacePhase()) return;
-    if (!m_enabled) return;
 
     //drawGlobalTimer checks if it should display in the current phase/mode
     drawGlobalTimer();
