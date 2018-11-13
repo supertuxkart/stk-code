814916a2a6184bafebf5e4aadae2bef75322f4b9
Deve <deveee@gmail.com>
2018-11-13 21:52:09 +0100

    Try to fix some crashes when closing android activity

--- src/main_android.cpp
+++ src/main_android.cpp
@@ -105,7 +105,7 @@ void android_main(struct android_app* app)
     // its state is remembered when the window is restored. We will use exit
     // call to make sure that all variables are cleared until a proper fix will 
     // be done.
-    exit(0);
+    _exit(0);
 }
 
 #endif
