7cc4c92b31b0367623f39bed32dacc8cfea651f3
Deve <deveee@gmail.com>
2018-11-14 20:59:00 +0100

    Fixed compilation with current mesa.
    It fixes #3557.

--- lib/glew/include/GL/glew.h
+++ lib/glew/include/GL/glew.h
@@ -93,7 +93,7 @@
 #if defined(__REGAL_H__)
 #error Regal.h included before glew.h
 #endif
-#if defined(__glext_h_) || defined(__GLEXT_H_)
+#if defined(__glext_h_) || defined(__GLEXT_H_) || defined(__gl_glext_h_)
 #error glext.h included before glew.h
 #endif
 #if defined(__gl_ATI_h_)
@@ -109,6 +109,7 @@
 #define __X_GL_H
 #define __glext_h_
 #define __GLEXT_H_
+#define __gl_glext_h_
 #define __gl_ATI_h_
 
 #if defined(_WIN32)
