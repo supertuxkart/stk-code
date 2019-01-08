b2c11c120bf289c43a0989c626ece454ecb6ea2f
Daniel Butum <danibutum@gmail.com>
2019-01-08 02:42:30 +0200

    Update config to use the new online servers. Remove hardcoded certificate as we will use Let's encrypt from now on (#3690)

--- src/online/http_request.cpp
+++ src/online/http_request.cpp
@@ -95,10 +95,10 @@ namespace Online
         m_parameters    = "";
         m_curl_code     = CURLE_OK;
         m_progress.setAtomic(0);
-        if (m_http_header == NULL)
+        if (m_http_header == nullptr)
         {
-            m_http_header = curl_slist_append(m_http_header,
-                "Host: addons.supertuxkart.net");
+            std::string Host = "Host: " + StringUtils::getHostNameFromURL(UserConfigParams::m_server_multiplayer);
+            m_http_header = curl_slist_append(m_http_header, Host.c_str());
         }
         m_disable_sending_log = false;
     }   // init
@@ -182,19 +182,8 @@ namespace Online
         if (m_url.substr(0, 8) == "https://")
         {
             // https, load certificate info
-            assert(m_http_header != NULL);
-            curl_easy_setopt(m_curl_session, CURLOPT_HTTPHEADER,
-                m_http_header);
-            const std::string& ci = file_manager->getCertLocation();
-            CURLcode error = curl_easy_setopt(m_curl_session, CURLOPT_CAINFO,
-                ci.c_str());
-            if (error != CURLE_OK)
-            {
-                Log::error("HTTPRequest", "Error setting CAINFO to '%s'",
-                    ci.c_str());
-                Log::error("HTTPRequest", "Error: '%s'.", error,
-                    curl_easy_strerror(error));
-            }
+            assert(m_http_header != nullptr);
+            curl_easy_setopt(m_curl_session, CURLOPT_HTTPHEADER, m_http_header);
             curl_easy_setopt(m_curl_session, CURLOPT_SSL_VERIFYPEER, 1L);
 #ifdef __APPLE__
             curl_easy_setopt(m_curl_session, CURLOPT_SSL_VERIFYHOST, 0L);
