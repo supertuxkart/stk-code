1855d4262b87dad395b1f9cdc75b1a7d2b5febb8
Benau <Benau@users.noreply.github.com>
2019-02-12 11:26:20 +0800

    Revert "Use xmlEncode for proper unicode handling"
    This reverts commit c5cf87071ce3b58d94cd3253deb02452d21f4547.
    
    We can remove utf8_encode in stk_addons api so it will handle
    utf8 characters in db correctly, as utf8_encode is only needed for
    a string encoded in ISO-8859-1.
    
    Fix #3670

--- src/online/http_request.hpp
+++ src/online/http_request.hpp
@@ -143,7 +143,7 @@ namespace Online
         void addParameter(const std::string &name,
                           const irr::core::stringw &value)
         {
-            std::string s = StringUtils::xmlEncode(value);
+            std::string s = StringUtils::wideToUtf8(value);
 
             // Call the template to escape strings properly
             addParameter(name, s.c_str());
