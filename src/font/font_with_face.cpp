3f209ceca62dbb8090a256288a8dfdf8e4d4499a
dumaosen <35170687+dumaosen@users.noreply.github.com>
2019-03-04 18:45:27 +0800

    Another patch of >=2k issue

--- src/font/font_with_face.cpp
+++ src/font/font_with_face.cpp
@@ -365,7 +365,7 @@ void FontWithFace::setDPI()
         // fix >= 2k scaling
         if (screen_height > 1300)
             scale = std::max(0, screen_width - 800) / 650.0f;
-        if (screen_width > 3400)
+        if (screen_height > 2000)
             scale = std::max(0, screen_width - 900) / 750.0f;
         // attempt to compensate for small screens
         if (screen_width < 1200)
