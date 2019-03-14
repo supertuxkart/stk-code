4e7526a712e1a5e6bef0b885c48b8b76bd498a61
Deve <deveee@gmail.com>
2019-03-14 21:18:30 +0100

    Replace dot/comma position in screen keyboard

--- src/guiengine/screen_keyboard.cpp
+++ src/guiengine/screen_keyboard.cpp
@@ -43,25 +43,25 @@ KeyboardLayout layout_lower =
             {{"q", "w", "e", "r", "t", "y", "u", "i", "o", "p"},
              {"Separator", "a", "s", "d", "f", "g", "h", "j", "k", "l", "Separator"},
              {"Shift", "z", "x", "c", "v", "b", "n", "m", "?", "Back"},
-             {"123", ".", "Space", ",", "Enter"}};
+             {"123", ",", "Space", ".", "Enter"}};
              
 KeyboardLayout layout_upper = 
             {{"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"},
              {"Separator", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Separator"},
              {"Shift", "Z", "X", "C", "V", "B", "N", "M", "!", "Back"},
-             {"123", ".", "Space", ",", "Enter"}};
+             {"123", ",", "Space", ".", "Enter"}};
                           
 KeyboardLayout layout_digits = 
             {{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"},
              {"Separator", "@", "#", "$", "%", "^", "&", "*", "(", ")", "Separator"},
              {"Shift", "-", "+", ":", ";", "\"", "\'", "/", "?", "Back"},
-             {"Text", ".", "Space", ",", "Enter"}};
+             {"Text", ",", "Space", ".", "Enter"}};
 
 KeyboardLayout layout_digits2 = 
             {{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"},
              {"Separator", "@", "[", "]", "{", "}", "~", "`", "\\", "|", "Separator"},
              {"Shift", "_", "=", ":", ";", "<", ">", "/", "!", "Back"},
-             {"Text", ".", "Space", ",", "Enter"}};
+             {"Text", ",", "Space", ".", "Enter"}};
 
 KeyboardLayoutProportions layout_proportions = 
             {{2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
