3b4c74654d9514c30bd111951604a9b23e4350c5
dumaosen <35170687+dumaosen@users.noreply.github.com>
2019-05-01 08:04:12 +0800

    Open bigger fonts in Android

--- src/main_android.cpp
+++ src/main_android.cpp
@@ -68,6 +68,9 @@ void override_default_params()
     // Enable screen keyboard
     UserConfigParams::m_screen_keyboard = 1;
     
+    // Set bigger fonts and buttons
+    UserConfigParams::m_bigger_fonts = true
+    
     // It shouldn't matter, but STK is always run in fullscreen on android
     UserConfigParams::m_fullscreen = true;
     
