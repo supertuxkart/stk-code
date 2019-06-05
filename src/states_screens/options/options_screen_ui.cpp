08c61bfb916629dfa429f3f7839374559c1e2a21
Deve <deveee@gmail.com>
2019-06-05 23:31:24 +0200

    Disable changing font size during race

--- src/states_screens/options/options_screen_ui.cpp
+++ src/states_screens/options/options_screen_ui.cpp
@@ -161,11 +161,14 @@ void OptionsScreenUI::init()
     }
     minimap_options->setValue(UserConfigParams::m_minimap_display);
     
+    bool in_game = StateManager::get()->getGameState() == GUIEngine::INGAME_MENU;
+    
     GUIEngine::SpinnerWidget* font_size = getWidget<GUIEngine::SpinnerWidget>("font_size");
     assert( font_size != NULL );
     
     font_size->setValue((int)roundf(UserConfigParams::m_fonts_size));
     m_prev_font_size = UserConfigParams::m_fonts_size;
+    font_size->setActive(!in_game);
 
     // ---- video modes
     CheckBoxWidget* splitscreen_method = getWidget<CheckBoxWidget>("split_screen_horizontally");
@@ -173,7 +176,6 @@ void OptionsScreenUI::init()
     splitscreen_method->setState(UserConfigParams::split_screen_horizontally);
 
     //Forbid changing this setting in game
-    bool in_game = StateManager::get()->getGameState() == GUIEngine::INGAME_MENU;
     splitscreen_method->setActive(!in_game);
 
     CheckBoxWidget* fps = getWidget<CheckBoxWidget>("showfps");
