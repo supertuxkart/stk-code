ce188085e7fa1565f64598bd501e219ed34787d5
dumaosen <dumaosen_main01@outlook.com>
2019-11-03 11:09:58 +0800

    Better size

--- src/guiengine/layout_manager.cpp
+++ src/guiengine/layout_manager.cpp
@@ -248,7 +248,7 @@ void LayoutManager::readCoords(Widget* self)
         if (self->getType() == WTYPE_DIV && self->m_show_bounding_box)
         {
             int padding = std::min(irr_driver->getActualScreenSize().Height,
-                                   irr_driver->getActualScreenSize().Width)  / 72.0f;
+                                   irr_driver->getActualScreenSize().Width)  / 36.0f;
             if (self->m_properties[PROP_DIV_PADDING].length() > 0)
                 padding = atoi(self->m_properties[PROP_DIV_PADDING].c_str());
             child_max_height += padding * 2;
@@ -324,7 +324,7 @@ void LayoutManager::applyCoords(Widget* self, AbstractTopLevelContainer* topLeve
     if (parent != NULL && parent->getType() == WTYPE_DIV && parent->m_show_bounding_box)
     {
         int padding = std::min(irr_driver->getActualScreenSize().Height,
-                               irr_driver->getActualScreenSize().Width)  / 72.0f;
+                               irr_driver->getActualScreenSize().Width)  / 36.0f;
         if (parent->m_properties[PROP_DIV_PADDING].length() > 0)
             padding = atoi(parent->m_properties[PROP_DIV_PADDING].c_str());
             
@@ -450,7 +450,8 @@ void LayoutManager::doCalculateLayout(PtrVector<Widget>& widgets, AbstractTopLev
 
         if (parent != NULL && parent->getType() == WTYPE_DIV && parent->m_show_bounding_box)
         {
-            int padding = GUIEngine::getFontHeight() / 2;
+            int padding = std::min(irr_driver->getActualScreenSize().Height,
+                                   irr_driver->getActualScreenSize().Width)  / 36.0f;
             if (parent->m_properties[PROP_DIV_PADDING].length() > 0)
                 padding = atoi(parent->m_properties[PROP_DIV_PADDING].c_str());
             
