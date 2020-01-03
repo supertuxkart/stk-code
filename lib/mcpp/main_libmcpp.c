db5d1420f94101e07414cc550066657d4693c4d1
Benau <Benau@users.noreply.github.com>
2020-01-03 12:46:35 +0800

    Add mcpp from github.com/ned14/mcpp

---
+++ lib/mcpp/main_libmcpp.c
@@ -0,0 +1,9 @@
+/* most simple sample source to use libmcpp */
+
+#include "mcpp_lib.h"
+
+int
+main (int argc, char *argv[])
+{
+    return mcpp_lib_main (argc, argv);
+}
---
+++ lib/mcpp/preproc.c
@@ -0,0 +1,9 @@
+/* preproc.c:   to "pre-preprocess" header files.   */
+
+#pragma MCPP preprocess
+
+#include    "system.H"
+#include    "internal.H"
+
+#pragma MCPP put_defines
+
