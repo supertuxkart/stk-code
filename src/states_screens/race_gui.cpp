751f93893b10dc2d1ef29e8964034e5819986c0d
riso <4726939+risostk@users.noreply.github.com>
2020-02-15 18:50:28 -0600

    correct grammar

--- src/states_screens/race_gui.cpp
+++ src/states_screens/race_gui.cpp
@@ -689,7 +689,7 @@ void RaceGUI::drawGlobalMiniMap()
                 // Get the rotation to rotate the icon frame
                 float rotation = atan2f(direction.getZ(),direction.getX());
                 if (track->getMinimapInvert())
-                {   // correction the direction
+                {   // correct the direction due to invert minimap for blue
                     rotation = rotation + M_PI;
                 }
                 rotation = -1.0f * rotation + 0.25f * M_PI; // icons-frame_arrow.png was rotated by 45 degrees
