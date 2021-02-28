3387c0bc7657f6b05f97e4feaa9dbd0aa2f8b5a1
Mary Strodl <ipadlover8322@gmail.com>
2021-02-28 16:50:36 -0500

    Add build script, remove crypto.hpp changes, disable opengl recorder on switch

---
+++ switch/make.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+OLD_PWD="$(pwd)"
+
+# Mac can prefix tool names with 'dkp-'
+if which dkp-pacman &>/dev/null; then
+  PACMAN=dkp-pacman
+elif which pacman &>/dev/null; then
+  PACMAN=pacman
+else
+  echo "Couldn't find pacman in PATH! Is it installed?"
+  echo "Please see https://devkitpro.org/wiki/devkitPro_pacman#Installing_devkitPro_Pacman for instructions to install it!"
+  exit 1
+fi
+
+# Install deps. --needed means don't reinstall if already installed
+$PACMAN -S --needed switch-dev \  # Compiler package group
+  devkitpro-pkgbuild-helpers \  # CMake architecture files and switchvars.sh
+  switch-curl switch-mbedtls \  # Networking
+  switch-freetype switch-libfribidi \  # Text rendering
+  switch-libogg switch-libvorbis \  # Audio
+  switch-libjpeg-turbo switch-libpng \  # Textures
+  switch-zlib switch-bzip2 \  # Compression
+  switch-physfs \  # Unsure
+  switch-pkg-config \  # Dep
+  switch-sdl2 switch-mesa switch-libdrm_nouveau \  # Display
+  libnx \  # FS
+  switch-tools # elf2nro
+
+# Users of MSYS2 or Arch Linux will already have Pacman installed but may not have the DKP repos on their system:
+if [ "T$?" -ne "T0" ]; then
+  echo "Failed to install packages! Did you add the repositories?"
+  echo "Please see https://devkitpro.org/wiki/devkitPro_pacman#Customising_Existing_Pacman_Install for instructions!"
+fi
+
+# Unclear why this isn't in lib path
+if [ ! -f "${DEVKITPRO}/portlibs/switch/lib/libpthread.a" ]; then
+  ln -s "${DEVKITPRO}/devkitA64/aarch64-none-elf/lib/libpthread.a" \
+    "${DEVKITPRO}/portlibs/switch/lib/libpthread.a"
+fi
+
+SWITCH_DIR=$(realpath "$(dirname "$0")")
+STK_DIR=$(dirname "${SWITCH_DIR}")
+
+# Some shells don't set BASH_SOURCE. Let's set it just in case:
+OLD_BASH_SOURCE="${BASH_SOURCE}"
+BASH_SOURCE="${DEVKITPRO}/switchvars.sh"
+source "${BASH_SOURCE}" # Sets environment variables needed for cross-compiling
+BASH_SOURCE="${OLD_BASH_SOURCE}"
+
+if [ ! -d "${STK_DIR}/libs/harfbuzz/cmake_build" ]; then
+  # Harfbuzz
+  echo "Compiling Harfbuzz"
+  mkdir "${STK_DIR}/libs/harfbuzz/cmake_build"
+  cd "${STK_DIR}/libs/harfbuzz/cmake_build"
+  cmake -G"Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="${DEVKITPRO}/switch.cmake" \
+    -DUSE_SWITCH=ON -DCMAKE_INSTALL_PREFIX="${PORTLIBS_PREFIX}"  \
+    ../
+  
+  make -j$(nproc)
+  make install
+fi
+
+if [ ! -d "${STK_DIR}/libs/openal/cmake_build" ]; then
+  # OpenAL
+  echo "Compiling OpenAL"
+  mkdir "${STK_DIR}/libs/openal/cmake_build"
+  cd "${STK_DIR}/libs/openal/cmake_build"
+  cmake -G"Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="${DEVKITPRO}/switch.cmake" \
+    -DUSE_SWITCH=ON -DALSOFT_UTILS=OFF -DLIBTYPE=STATIC -DALSOFT_EXAMPLES=OFF \
+    -DALSOFT_REQUIRE_SDL2=ON -DALSOFT_BACKEND_SDL2=ON \
+    -DSDL2_INCLUDE_DIR="${PORTLIBS_PREFIX}/include" \
+    -DCMAKE_INSTALL_PREFIX="${PORTLIBS_PREFIX}"  \
+    ../
+
+  make -j$(nproc)
+  make install
+fi
+
+echo "Compiling STK"
+
+mkdir "${STK_DIR}/cmake_build"
+cd "${STK_DIR}/cmake_build"
+
+cmake -G"Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="${DEVKITPRO}/switch.cmake" \
+    -DUSE_SWITCH=ON \
+    -DCMAKE_INSTALL_PREFIX=/  \
+    ../
+
+make -j$(nproc)
+make install DESTDIR=./install
+
+# Build nro (executable for switch)
+"${SWITCH_DIR}/package.sh"
+
+echo "Building package"
+
+rm -rf sdcard
+mkdir sdcard
+# Move data over
+mv install/share/supertuxkart/data sdcard/stk-data
+# Add executable
+mkdir sdcard/switch
+mv bin/stk.nro sdcard/switch/stk.nro
+
+echo "Compressing"
+
+# Zip up actual release:
+cd scard
+ZIP_PATH="${STK_DIR}/cmake_build/bin/SuperTuxKart-Switch-${PROJECT_VERSION}.zip"
+zip -r "${ZIP_PATH}" .
+
+# Recover old pwd
+cd $OLD_PWD
+
+echo "Done. Package available at ${ZIP_PATH}"
