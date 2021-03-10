426f3f5a92ee9838a7d5dfb34ec1c52aa3bfeb67
Mary Strodl <ipadlover8322@gmail.com>
2021-03-09 21:27:53 -0500

    RichPresence: fix compiler warning

--- src/io/rich_presence.hpp
+++ src/io/rich_presence.hpp
@@ -19,12 +19,12 @@ namespace RichPresenceNS {
         bool m_connected;
         bool m_ready;
         time_t m_last;
-        std::thread* m_thread;
 #ifdef WIN32
         HANDLE m_socket;
 #else
         int m_socket;
 #endif
+        std::thread* m_thread;
         bool tryConnect(std::string path);
         bool doConnect();
         void terminate();
