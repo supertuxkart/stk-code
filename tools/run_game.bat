0d84350ab73a46ccd329d1bd97898a39e8c14110
Benau <Benau@users.noreply.github.com>
2021-05-01 01:06:11 +0800

    Create Windows installer with GitHub Actions for tagged build

--- tools/run_game.bat
+++ tools/run_game.bat
@@ -2,12 +2,12 @@
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
 
 if %OS%==32BIT (
-Pushd %~dp0\stk-code\build-mingw\bin\
+Pushd %~dp0\stk-code\build-i686\bin\
 supertuxkart.exe
 popd
 )
 if %OS%==64BIT (
-Pushd %~dp0\stk-code\build-mingw64\bin\
+Pushd %~dp0\stk-code\build-x86_64\bin\
 supertuxkart.exe
 popd
 )
