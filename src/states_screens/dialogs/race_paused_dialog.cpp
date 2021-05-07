4a2839260b5bed1c5d8736001b760446e4585320
mrkubax10 <mrkubax10@onet.pl>
2021-05-07 13:17:24 +0200

    Fixed issue #4243

--- src/states_screens/dialogs/race_paused_dialog.cpp
+++ src/states_screens/dialogs/race_paused_dialog.cpp
@@ -123,7 +123,23 @@ RacePausedDialog::RacePausedDialog(const float percentWidth,
     {
         World::getWorld()->schedulePause(WorldStatus::IN_GAME_MENU_PHASE);
     }
-
+    if(RaceManager::get()->isBattleMode() || RaceManager::get()->isCTFMode())
+    {
+        getWidget<IconButtonWidget>("backbtn")->setLabel(_("Back to Battle"));
+        getWidget<IconButtonWidget>("newrace")->setLabel(_("Setup New Game"));
+        getWidget<IconButtonWidget>("restart")->setLabel(_("Restart Battle"));
+        getWidget<IconButtonWidget>("exit")->setLabel(_("Exit Battle"));
+    }
+    else
+    {
+        getWidget<IconButtonWidget>("backbtn")->setLabel(_("Back to Race"));
+        getWidget<IconButtonWidget>("newrace")->setLabel(_("Setup New Race"));
+        getWidget<IconButtonWidget>("restart")->setLabel(_("Restart Race"));
+        IconButtonWidget* endrace=getWidget<IconButtonWidget>("endrace");
+        if(endrace)
+            endrace->setLabel(_("Give Up Race"));
+        getWidget<IconButtonWidget>("exit")->setLabel(_("Exit Race"));
+    }
 #ifndef MOBILE_STK
     if (m_text_box && UserConfigParams::m_lobby_chat)
         m_text_box->setFocusForPlayer(PLAYER_ID_GAME_MASTER);
