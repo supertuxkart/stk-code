d0f4c8ffee30304f5b25d50d4fc71e8b03f67e94
Benau <Benau@users.noreply.github.com>
2021-09-28 15:48:26 +0800

    Update download assets size

--- src/utils/download_assets_size.hpp
+++ src/utils/download_assets_size.hpp
@@ -23,7 +23,7 @@
 inline unsigned getDownloadAssetsSize()
 {
     // Todo: generated from some sed script
-    unsigned stk_assets_size = 207610940;
+    unsigned stk_assets_size = 207783367;
     return stk_assets_size;
 }
 #endif
