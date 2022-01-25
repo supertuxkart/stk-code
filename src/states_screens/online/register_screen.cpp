3d4cb58af5dad6ac9e933df71442925bcdfade1f
Lois Doyenard <ldoyenard@emi.u-bordeaux.fr>
2022-01-25 10:48:38 +0100

    Accept emails with format e.email (from #4718)

--- src/states_screens/online/register_screen.cpp
+++ src/states_screens/online/register_screen.cpp
@@ -386,7 +386,7 @@ void RegisterScreen::doRegister()
         m_info_widget->setText(_("Email has to be between 5 and 254 characters long!"), false);
     }
     else if (  email.find(L"@")== -1 || email.find(L".")== -1 ||
-              (email.findLast(L'.') - email.findLast(L'@') <= 2 ) ||
+              (email.findLast(L'.') - email.findLast(L'@') <= 1 ) ||
                 email.findLast(L'@')==0 || email[(email.size())-1]=='.')
     {
         m_info_widget->setText(_("Email is invalid!"), false);
