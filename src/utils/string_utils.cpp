dcdc66b8f5a2e8e75d6fce2676fdd98d5af4db49
Benau <Benau@users.noreply.github.com>
2022-02-25 11:13:27 +0800

    Fix undefined behavior in C++20, see #4735

--- src/utils/string_utils.cpp
+++ src/utils/string_utils.cpp
@@ -832,7 +832,8 @@ namespace StringUtils
             if (utf32[i] >= 128 || utf32[i] == '&' || utf32[i] == '<' ||
                 utf32[i] == '>' || utf32[i] == '\"' || utf32[i] == ' ')
             {
-                output << "&#x" << std::hex << std::uppercase << utf32[i] << ";";
+                unsigned code = (unsigned)utf32[i];
+                output << "&#x" << std::hex << std::uppercase << code << ";";
             }
             else
             {
