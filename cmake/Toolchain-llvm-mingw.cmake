c0045886544effe8845af7d6a54b44ce284e1ab6
Benau <Benau@users.noreply.github.com>
2022-05-10 10:05:01 +0800

    Allow llvm mingw toolchain to be used in windows

--- cmake/Toolchain-llvm-mingw.cmake
+++ cmake/Toolchain-llvm-mingw.cmake
@@ -2,12 +2,15 @@
 # cmake .. -DLLVM_ARCH=aarch64 i686 or x86_64 -DLLVM_PREFIX=/path/to/llvm-mingw-prefix -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-llvm-mingw.cmake
 
 # the name of the target operating system
+if(WIN32)
+SET(PATH_EXE .exe)
+endif()
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER ${LLVM_PREFIX}/bin/${LLVM_ARCH}-w64-mingw32-clang)
-SET(CMAKE_CXX_COMPILER ${LLVM_PREFIX}/bin/${LLVM_ARCH}-w64-mingw32-clang++)
-SET(CMAKE_RC_COMPILER ${LLVM_PREFIX}/bin/${LLVM_ARCH}-w64-mingw32-windres)
+SET(CMAKE_C_COMPILER ${LLVM_PREFIX}/bin/${LLVM_ARCH}-w64-mingw32-clang${PATH_EXE})
+SET(CMAKE_CXX_COMPILER ${LLVM_PREFIX}/bin/${LLVM_ARCH}-w64-mingw32-clang++${PATH_EXE})
+SET(CMAKE_RC_COMPILER ${LLVM_PREFIX}/bin/${LLVM_ARCH}-w64-mingw32-windres${PATH_EXE})
 SET(CMAKE_SYSTEM_PROCESSOR ${LLVM_ARCH})
 SET(CMAKE_BUILD_TYPE RelWithDebInfo)
 set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++ -Wl,-pdb=")
