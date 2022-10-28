2707234ada0e5523f57dad56dc7b795a87f155b8
Benau <Benau@users.noreply.github.com>
2022-10-28 13:39:06 +0800

    Add armv7 and aarch64 detection in run_game.bat, see #4805

--- tools/run_game.bat
+++ tools/run_game.bat
@@ -1,13 +1,25 @@
 @echo off
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
 
-if %OS%==32BIT (
+if %PROCESSOR_ARCHITECTURE%==x86 (
 Pushd %~dp0\stk-code\build-i686\bin\
 supertuxkart.exe
 popd
 )
-if %OS%==64BIT (
+
+if %PROCESSOR_ARCHITECTURE%==AMD64 (
 Pushd %~dp0\stk-code\build-x86_64\bin\
+supertuxkart.exe
+popd
+)
+
+if %PROCESSOR_ARCHITECTURE%==ARM64 (
+Pushd %~dp0\stk-code\build-aarch64\bin\
+supertuxkart.exe
+popd
+)
+
+if %PROCESSOR_ARCHITECTURE%==ARM (
+Pushd %~dp0\stk-code\build-armv7\bin\
 supertuxkart.exe
 popd
 )
