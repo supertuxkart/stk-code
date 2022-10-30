440def908bf07ae7f94eb17d9d4bee413605a3bf
Benau <Benau@users.noreply.github.com>
2022-10-30 15:29:11 +0800

    Fix missing stk.nro

--- switch/package.sh
+++ switch/package.sh
@@ -1,2 +1,2 @@
 nacptool --create "SuperTuxKart" "Many People" "${PROJECT_VERSION}" control.nacp
-elf2nro bin/supertuxkart bin/stk.nro --nacp=control.nacp --icon=../switch/supertuxkart_256.jpg
+elf2nro bin/supertuxkart.elf bin/stk.nro --nacp=control.nacp --icon=../switch/supertuxkart_256.jpg
