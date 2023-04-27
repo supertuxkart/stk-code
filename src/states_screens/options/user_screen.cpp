0c9a136d7c0aaae0a3f71854fd30af78e3e1b13b
QwertyChouskie <asdfghrbljzmkd@outlook.com>
2023-04-27 00:58:54 -0700

    UI/UX: Use password recovery dialog instead of opening webpage

--- src/states_screens/options/user_screen.cpp
+++ src/states_screens/options/user_screen.cpp
@@ -404,8 +404,7 @@ void BaseUserScreen::eventCallback(Widget* widget,
     }
     else if (name == "password_reset")
     {
-        // Open password reset page
-        Online::LinkHelper::openURL(stk_config->m_password_reset_url);
+        new RecoveryDialog();
     }
     else if (name == "options")
     {
@@ -428,10 +427,6 @@ void BaseUserScreen::eventCallback(Widget* widget,
         {
             // EscapePressed will pop this screen.
             StateManager::get()->escapePressed();
-        }
-        else if (button == "recover")
-        {
-            new RecoveryDialog();
         }
         else if (button == "rename")
         {
