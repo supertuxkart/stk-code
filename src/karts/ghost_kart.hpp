f8db600d098390f4fe1723546e831aeac8812992
Alayan <25536748+Alayan-stk-2@users.noreply.github.com>
2023-12-14 16:55:53 +0100

    Fix #4965

--- src/karts/ghost_kart.hpp
+++ src/karts/ghost_kart.hpp
@@ -50,6 +50,8 @@ private:
 
     unsigned int                             m_last_egg_idx;
 
+    bool m_finish_computed;
+
     // ----------------------------------------------------------------------------
     /** Compute the time at which the ghost finished the race */
     void          computeFinishTime();
@@ -84,11 +86,9 @@ public:
     // ------------------------------------------------------------------------
     /** Returns the speed of the kart in meters/second. */
     virtual float getSpeed() const OVERRIDE;
-
     // ------------------------------------------------------------------------
     /** Returns the finished time for a ghost kart. */
-    float  getGhostFinishTime() { computeFinishTime(); return m_finish_time; }
-
+    float  getGhostFinishTime();
     // ------------------------------------------------------------------------
     /** Returns the time at which the kart was at a given distance.
       * Returns -1.0f if none */
