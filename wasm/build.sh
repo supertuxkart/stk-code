2a108ac922ba25368d8b80bf14eccaeec5aea639
ading2210 <ading2019@gmail.com>
2024-05-29 11:15:14 -0400

    load game data from tar.gz file

--- wasm/build.sh
+++ wasm/build.sh
@@ -4,7 +4,6 @@ CORE_COUNT="$(nproc --all)"
 BASE_DIR="$(realpath "$(dirname "$0")")"
 SRC_DIR="$(dirname "$BASE_DIR")"
 WEB_DIR="$BASE_DIR/web"
-DATA_DIR="$SRC_DIR/game"
 BUILD_DIR="$SRC_DIR/cmake_build"
 EMSDK_DIR="$BASE_DIR/emsdk"
 PACKAGER="$EMSDK_DIR/upstream/emscripten/tools/file_packager.py"
@@ -17,6 +16,10 @@ source $EMSDK_DIR/emsdk_env.sh
 emcmake cmake .. -DNO_SHADERC=on
 make -j4
 
+echo "packing game files"
 mkdir -p "$WEB_DIR/game"
 cp ./bin/* "$WEB_DIR/game/"
+tar -cf - -C "$SRC_DIR/data" . | gzip -9 - > "$WEB_DIR/game/data.tar.gz"
+
+echo "applying patches"
 python3 $BASE_DIR/patch_js.py $BASE_DIR/fragments $WEB_DIR/game/supertuxkart.js
---
+++ wasm/web/script.js
@@ -0,0 +1,32 @@
+import pako from "https://cdn.jsdelivr.net/npm/pako@2.1.0/+esm";
+import jsUntar from "https://cdn.jsdelivr.net/npm/js-untar@2.0.0/+esm";
+
+async function extract_tar(url, fs_path) {
+  //download tar file from server and decompress
+  let r = await fetch(url);
+  let compressed = await r.arrayBuffer();
+  let decompressed = pako.inflate(compressed);
+  let files = await jsUntar(decompressed.buffer);
+  
+  //save each file to the emscripten filesystem
+  for (let file of files) {
+    let relative_path = file.name.substring(1);
+    let out_path = fs_path + relative_path;
+    console.log(file);
+    if (out_path.endsWith("/")) {
+      FS.mkdir(out_path);
+    }
+    else {
+      let array = new Uint8Array(file.buffer);
+      FS.writeFile(out_path, array);
+    }
+  }
+}
+
+async function load_data() {
+  await extract_tar("/game/data.tar.gz", "/data");
+}
+
+globalThis.pako = pako;
+globalThis.jsUntar = jsUntar;
+globalThis.load_data = load_data;
