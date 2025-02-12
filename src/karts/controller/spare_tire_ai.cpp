8f889d41882a0e460dae30eab83a0e173a45a66d
bart <bart@bbun>
2025-02-12 17:41:57 +0100

    changes to three strike battle

--- src/karts/controller/spare_tire_ai.cpp
+++ src/karts/controller/spare_tire_ai.cpp
@@ -142,20 +142,11 @@ void SpareTireAI::crashed(const AbstractKart *k)
     // Nothing happen when two spare tire karts crash each other
     if (dynamic_cast<const SpareTireAI*>(k->getController()) != NULL) return;
 
-    // Tell players that they can have at most 3 lives
+    // Add a life
     RaceGUIBase* r = World::getWorld()->getRaceGUI();
-    if (m_tsb_world->getKartLife(k->getWorldKartId()) == 3)
-    {
-        if (r)
-            r->addMessage(_("You can have at most 3 lives!"), k, 2.0f);
-    }
-    // Otherwise add one life for that kart 
-    else
-    {
-        m_tsb_world->addKartLife(k->getWorldKartId());
-        if (r)
-            r->addMessage(_("+1 life."), k, 2.0f);
-    }
+    m_tsb_world->addKartLife(k->getWorldKartId());
+    if (r)
+        r->addMessage(_("+1 life."), k, 2.0f);
     unspawn();
 
 }   // crashed
