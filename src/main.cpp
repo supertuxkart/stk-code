84d0e3dc62a0bb01cc5f2a79047335ce21496319
kimden <23140380+kimden@users.noreply.github.com>
2025-03-30 17:42:27 +0400

    Make loading screen icons fit ~40% of it (#5383)
    I don't know how to get the number of icons in advance, nor I can make proper animations, so for now the jump in size is like that.
    I will add more commits if I find out how.

--- src/main.cpp
+++ src/main.cpp
@@ -1889,6 +1889,7 @@ void clearGlobalVariables()
 //=============================================================================
 void initRest()
 {
+    GUIEngine::reserveLoadingIcons(2);
     SP::setMaxTextureSize();
     irr_driver = new IrrDriver();
 
@@ -2310,6 +2311,7 @@ int main(int argc, char *argv[])
         wiimote_manager = new WiimoteManager();
 #endif
 
+        GUIEngine::reserveLoadingIcons(4);
         int parent_pid;
         bool has_parent_process = false;
         if (CommandLine::has("--parent-process", &parent_pid))
