c0af0729fdd1aa1ef1e69eef5c6ccb4c96fa9ca6
Benau <Benau@users.noreply.github.com>
2025-09-28 14:49:16 +0000

    Fix assertion when using turnip driver

--- lib/graphics_engine/src/ge_vulkan_draw_call.cpp
+++ lib/graphics_engine/src/ge_vulkan_draw_call.cpp
@@ -1613,7 +1613,7 @@ void GEVulkanDrawCall::createVulkanData()
     // Use VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT | VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
     // or a staging buffer when buffer is small
     m_dynamic_data = new GEVulkanDynamicBuffer(flags,
-        extra_size + sizeof(GEVulkanCameraUBO) + sizeof(GEGlobalLightBuffer),
+        extra_size + getLightDataOffset() + sizeof(GEGlobalLightBuffer),
         GEVulkanDriver::getMaxFrameInFlight() + 1,
         GEVulkanDynamicBuffer::supportsHostTransfer() ? 0 :
         GEVulkanDriver::getMaxFrameInFlight() + 1);
