supertuxkart/stk-code

commit 32c5ac052edd09598824407adce2d026c1afb497

Fix #3290
- Mostly remove the duplication between main.cpp's init and irr_driver.cpp's applyResolutionSettings by means of a new function
- Add a new function to ParticleKindManager to preload explosions.
- Increase the length of the function separators in irr_driver.cpp from 80 columns to 96 columns
- Remove a pointless define guard (the entire function was wrapped in the same define guard)
- Add a comment about the effect of the HQ texture setting

src/main.cpp
@@ -2373,7 +2373,7 @@ int main(int argc, char *argv[])
         wiimote_manager = new WiimoteManager();
 #endif
 
-        GUIEngine::reserveLoadingIcons(4);
+        GUIEngine::reserveLoadingIcons(1);
         int parent_pid;
         bool has_parent_process = false;
         if (CommandLine::has("--parent-process", &parent_pid))
@@ -2383,22 +2383,6 @@ int main(int argc, char *argv[])
         }
         else
             main_loop = new MainLoop(0/*parent_pid*/);
-        material_manager->loadMaterial();
-
-        // Preload the explosion effects (explode.png)
-        ParticleKindManager::get()->getParticles("explosion.xml");
-        ParticleKindManager::get()->getParticles("explosion_bomb.xml");
-        ParticleKindManager::get()->getParticles("explosion_cake.xml");
-        ParticleKindManager::get()->getParticles("jump_explosion.xml");
-
-        GUIEngine::addLoadingIcon( irr_driver->getTexture(FileManager::GUI_ICON,
-                                                          "options_video.png"));
-        kart_properties_manager -> loadAllKarts    ();
-        kart_properties_manager->onDemandLoadKartTextures(
-            { UserConfigParams::m_default_kart }, false/*unload_unused*/);
-        OfficialKarts::load();
-        handleXmasMode();
-        handleEasterEarMode();
 
         // Needs the kart and track directories to load potential challenges
         // in those dirs, so it can only be created after reading tracks
@@ -2413,35 +2397,14 @@ int main(int argc, char *argv[])
 
         GUIEngine::addLoadingIcon( irr_driver->getTexture(FileManager::GUI_ICON,
                                                           "gui_lock.png"  ) );
-        ProjectileManager::get()->loadData();
 
-        // Both item_manager and powerup_manager load models and therefore
-        // textures from the model directory. To avoid reading the
-        // materials.xml twice, we do this here once for both:
-        file_manager->pushTextureSearchPath(file_manager->getAsset(FileManager::MODEL,""), "models");
-        const std::string materials_file =
-            file_manager->getAsset(FileManager::MODEL,"materials.xml");
-        if(materials_file!="")
-        {
-            // Some of the materials might be needed later, so just add
-            // them all permanently (i.e. as shared). Adding them temporary
-            // will actually not be possible: powerup_manager adds some
-            // permanent icon materials, which would (with the current
-            // implementation) make the temporary materials permanent anyway.
-            material_manager->addSharedMaterial(materials_file);
-        }
-        Referee::init();
-        powerup_manager->loadPowerupsModels();
-        ItemManager::loadDefaultItemMeshes();
+        // Set of loading steps common between the first game launch and
+        // reloading to apply a new resolution.
+        irr_driver->commonInit();
 
-        GUIEngine::addLoadingIcon( irr_driver->getTexture(FileManager::GUI_ICON,
-                                                          "gift.png")       );
-
-        attachment_manager->loadModels();
-        file_manager->popTextureSearchPath();
-
-        GUIEngine::addLoadingIcon( irr_driver->getTexture(FileManager::GUI_ICON,
-                                                          "banana.png")    );
+        OfficialKarts::load();
+        handleXmasMode();
+        handleEasterEarMode();
 
         //handleCmdLine() needs InitTuxkart() so it can't be called first
         if (!handleCmdLine(!server_config.empty(), has_parent_process))

src/graphics/graphical_presets.cpp
@@ -179,6 +179,7 @@ namespace GraphicalPresets
     // --------------------------------------------------------------------------------------------
     int getImageQuality()
     {
+        // If high definition texture is true, the max texture size is 2048. Else, it's 512.
         if (UserConfigParams::m_anisotropic == 4 &&
             (UserConfigParams::m_high_definition_textures & 0x01) == 0x00 &&
             UserConfigParams::m_hq_mipmap == false)

src/graphics/irr_driver.hpp
@@ -202,7 +202,8 @@ private:
     // ------------------------------------------------------------------------
     void resizeWindow();
 public:
-    void doScreenShot();    
+    void doScreenShot();
+    void commonInit();
 public:
          IrrDriver();
         ~IrrDriver();

src/graphics/particle_kind_manager.cpp
@@ -73,6 +73,15 @@ void ParticleKindManager::cleanUpTrackSpecificGfx()
 }
 
 // ----------------------------------------------------------------------------
+void ParticleKindManager::preloadExplosions()
+{
+    getParticles("explosion.xml");
+    getParticles("explosion_bomb.xml");
+    getParticles("explosion_cake.xml");
+    getParticles("jump_explosion.xml");
+}   // preloadExplosions
+
+// ----------------------------------------------------------------------------
 
 ParticleKind* ParticleKindManager::getParticles(const std::string &name)
 {

src/graphics/particle_kind_manager.hpp
@@ -48,6 +48,7 @@ public:
     ParticleKind* getParticles(const std::string &name);
 
     bool   isGlobalParticleMaterial(Material* m) const;
+    void   preloadExplosions();
     void   cleanUpTrackSpecificGfx();
     void   cleanup();
 

commit bd01b0c9d16d804c1c7267cad8330347386a79b5

Fix a small regression
Karts that should be unlocked in Story Mode were not properly locked. The regression was introduced in 32c5ac0, because the kart information was loaded after the UnlockManager. Moving the kart loading back before fixes the issue.

src/main.cpp
@@ -2383,6 +2383,14 @@ int main(int argc, char *argv[])
         else
             main_loop = new MainLoop(0/*parent_pid*/);
 
+        // Set of loading steps common between the first game launch and
+        // reloading to apply a new resolution.
+        irr_driver->commonInit();
+
+        OfficialKarts::load();
+        handleXmasMode();
+        handleEasterEarMode();
+
         // Needs the kart and track directories to load potential challenges
         // in those dirs, so it can only be created after reading tracks
         // and karts.
@@ -2396,14 +2404,6 @@ int main(int argc, char *argv[])
 
         GUIEngine::addLoadingIcon( irr_driver->getTexture(FileManager::GUI_ICON,
                                                           "gui_lock.png"  ) );
-
-        // Set of loading steps common between the first game launch and
-        // reloading to apply a new resolution.
-        irr_driver->commonInit();
-
-        OfficialKarts::load();
-        handleXmasMode();
-        handleEasterEarMode();
 
         //handleCmdLine() needs InitTuxkart() so it can't be called first
         if (!handleCmdLine(!server_config.empty(), has_parent_process))