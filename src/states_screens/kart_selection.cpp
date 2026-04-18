c04cc757a723210be2f10e08f00b28935f53e6cc
Alexander A. Klimov <a@kli.mov>
2026-04-18 14:38:09 +0200

    Remember THAT player 1 selected a random kart, not WHICH one
    If player 1 selected a random kart, store "randomkart" to pre-select latter next time.

--- src/states_screens/kart_selection.cpp
+++ src/states_screens/kart_selection.cpp
@@ -1363,6 +1363,11 @@ void KartSelectionScreen::allPlayersDone()
     {
         std::string selected_kart = m_kart_widgets[n].m_kart_internal_name;
 
+        if (n == PLAYER_ID_GAME_MASTER)
+        {
+            UserConfigParams::m_default_kart = selected_kart;
+        }
+
         if (selected_kart == RANDOM_KART_ID)
         {
             // don't select an already selected kart
@@ -1399,11 +1404,6 @@ void KartSelectionScreen::allPlayersDone()
                     break;
                 }
             }
-        }
-        
-        if (n == PLAYER_ID_GAME_MASTER)
-        {
-            UserConfigParams::m_default_kart = selected_kart;
         }
 
         RaceManager::get()->setPlayerKart(n, selected_kart);
