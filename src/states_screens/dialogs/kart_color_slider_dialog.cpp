73e56423530ae71d28a8c49ea0e71611ea761b90
GustaLOLE <gustavo.barreira92@gmail.com>
2026-06-22 14:57:14 -0300

    Revert last commit and increase the scale coefficient to 50.0f
     - It's smoother now and the bigger scale coefficient make the if-cases unnecessary

--- src/states_screens/dialogs/kart_color_slider_dialog.cpp
+++ src/states_screens/dialogs/kart_color_slider_dialog.cpp
@@ -105,17 +105,7 @@ void KartColorSliderDialog::beforeAddingWidgets()
     core::matrix4 model_location;
 
     // scaling the view basing in the kart length
-    float scale = 35.0f / kart_model.getLength();
-
-    // if the kart is too short, use a predefined scale
-    if (kart_model.getLength() < 1.45f)
-    {
-        scale = 35.0f;
-    }
-    else if (kart_model.getLength() < 1.75f)
-    {
-        scale = 30.0f;
-    }
+    float scale = 50.0f / kart_model.getLength();
 
     model_location.setScale(core::vector3df(scale, scale, scale));
 
