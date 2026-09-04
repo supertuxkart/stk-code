27ce54f9b65cb34861a3af793159bce912558b1d
Alayan <25536748+Alayan-stk-2@users.noreply.github.com>
2026-09-04 14:14:49 +0200

    Improve the register screen error messages
    - Better message when the username is missing
    - Better message when the username is too long (display the limit and the current count to help the user fit under it without excessive guesswork)
    - Also fix the signed-unsigned comparison warning

--- src/states_screens/online/register_screen.cpp
+++ src/states_screens/online/register_screen.cpp
@@ -305,14 +305,15 @@ void RegisterScreen::doRegister()
     if (local_name.empty())
     {
         m_info_widget->setErrorColor();
-        m_info_widget->setText(_("User name cannot be empty."), false);
+        m_info_widget->setText(_("Your username cannot be empty!"), false);
         return;
     }
     // Avoid excessively long names to prevent various UI overflows (limit adjustable in config.xml)
-    else if (local_name.size() > UserConfigParams::m_local_username_length)
+    else if (local_name.size() > (unsigned int)UserConfigParams::m_local_username_length)
     {
     	m_info_widget->setErrorColor();
-        m_info_widget->setText(_("User name cannot be too long."), false);
+        m_info_widget->setText(_("Your username cannot exceed %i characters (current: %i).",
+            UserConfigParams::m_local_username_length, (int)local_name.size()), false);
         return;
      }
 
